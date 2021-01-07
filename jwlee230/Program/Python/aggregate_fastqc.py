f254e488be7ece44eabec99feb1f82be8ac61e19
Jaewoong Lee <230@fumire.moe>
2021-01-07 15:22:29 +0900

    Add FastQC (Issue #2)

---
+++ jwlee230/Program/Python/aggregate_fastqc.py
@@ -0,0 +1,65 @@
+"""
+aggregate_fastqc.py: aggregate FastQC results into a single figure
+"""
+import argparse
+import zipfile
+import matplotlib
+import matplotlib.pyplot
+import pandas
+import step00
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Input ZIP files", type=str, nargs="+")
+    parser.add_argument("output", help="Output PNG file", type=str)
+
+    args = parser.parse_args()
+
+    if not args.output.endswith(".png"):
+        raise ValueError("Output file must end with .png!!")
+
+    args.input.sort()
+
+    for file_name in args.input:
+        with zipfile.ZipFile(file_name) as zip_file:
+            zip_file.extract(list(filter(lambda x: x.endswith("summary.txt"), zip_file.namelist()))[0], "/tmp/FastQC")
+
+    raw_data = list()
+    for directory in step00.directory_list("/tmp/FastQC"):
+        with open(step00.file_list(directory)[0], "r") as f:
+            for line in f.readlines():
+                split_line = line.strip().split("\t")
+                raw_data.append(split_line)
+
+    data = pandas.DataFrame(raw_data, columns=["Result", "Item", "File"])
+
+    item_list = sorted(set(data["Item"]))
+    file_list = sorted(set(data["File"]))
+
+    matplotlib.use("Agg")
+    matplotlib.rcParams.update({"font.size": 30})
+
+    fig, ax = matplotlib.pyplot.subplots(figsize=(64, 18))
+
+    for i, f in enumerate(file_list):
+        for j, item in enumerate(item_list):
+            result = list(data.loc[(data["Item"] == item) & (data["File"] == f)]["Result"])[0]
+
+            if result == "PASS":
+                matplotlib.pyplot.scatter(i, j, s=20, marker="o", c="g")
+            elif result == "WARN":
+                matplotlib.pyplot.scatter(i, j, s=20, marker="^", c="y")
+            elif result == "FAIL":
+                matplotlib.pyplot.scatter(i, j, s=20, marker="X", c="r")
+            else:
+                raise Exception("Something went wrong!!")
+
+    matplotlib.pyplot.xticks([])
+    matplotlib.pyplot.yticks(range(len(item_list)), item_list)
+
+    matplotlib.pyplot.xlabel("Files")
+    matplotlib.pyplot.ylabel("Tests")
+
+    fig.savefig(args.output)
+    matplotlib.pyplot.close(fig)
