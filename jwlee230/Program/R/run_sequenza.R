ca57a1ff88177e0985a9e8f6b4c2d88345fb1c79
Jaewoong Lee <230@fumire.moe>
2021-01-29 18:51:34 +0900

    Add sequenza (Issue #6 is perfectly solved)

--- jwlee230/Program/R/run_sequenza.R
+++ jwlee230/Program/R/run_sequenza.R
@@ -1,7 +1,10 @@
 rm(list = ls())
 
 library(optparse)
-option_list = list(make_option(c("-s", "--sampleid"), type="character", default=NULL, help="sample name", metavar="character"), make_option(c("-i", "--input"), type="character", default=NULL, help="input .seqz.gz file", metavar="character"), make_option(c("-o", "--outdir"), type="character", default=NULL, help="output directory", metavar="character"), make_option(c("-p", "--parallel"), type="integer", default=NULL, help="output directory", metavar="character"))
+option_list = list(make_option(c("-s", "--sampleid"), type="character", default=NULL, help="sample name", metavar="character"),
+                   make_option(c("-i", "--input"), type="character", default=NULL, help="input .seqz.gz file", metavar="character"),
+                   make_option(c("-o", "--outdir"), type="character", default=NULL, help="output directory", metavar="character"),
+                   make_option(c("-p", "--parallel"), type="integer", default=NULL, help="parallel number of CPUs", metavar="character"))
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
@@ -9,16 +12,17 @@ main <- function(sampleid, input, outdir, parallel)
 {
     library(sequenza)
 
-    test <- sequenza.extract(input, assembly="hg19", parallel=parallel)
-    CP <- sequenza.fit(test, mc.cores=parallel)
-    sequenza.results(sequenza.extract=test, cp.table=CP, sample.id=sampleid, out.dir=outdir)
+    chrs = c(1:22, "X", "Y")
+    chrs <- paste0("chr", chrs)
+
+    test <- sequenza.extract(file=input, chromosome.list=chrs, parallel=parallel)
+    CP <- sequenza.fit(sequenza.extract=test, mc.cores=parallel, chromosome.list=chrs, XY=c(X="chrX", Y="chrY"))
+    sequenza.results(sequenza.extract=test, cp.table=CP, sample.id=sampleid, out.dir=outdir, chromosome.list=chrs, XY=c(X="chrX", Y="chrY"))
 }
 
 if (length(opt) == 5)
 {
     main(opt$sampleid, opt$input, opt$outdir, opt$parallel);
-}
-else
-{
+} else {
     print_help(opt_parser)
 }
