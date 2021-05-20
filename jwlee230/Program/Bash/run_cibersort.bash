7a26624547642260072ac1abe53a4760e4b18b05
Jaewoong Lee <230@fumire.moe>
2021-05-20 16:52:42 +0900

    Remove memory limitation: docker will do

--- jwlee230/Program/Bash/run_cibersort.bash
+++ jwlee230/Program/Bash/run_cibersort.bash
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 if (($# == 3)); then
     R CMD Rserve --no-save
     unzip /Tools/CIBERSORT.zip
-    java -Xmx3g -Xms3g -jar CIBERSORT/CIBERSORT.jar -M $1 -B $2 > $3
+    java -jar CIBERSORT/CIBERSORT.jar -M $1 -B $2 > $3
     sed -i -e '1,6d' $3
 else
     echo "This script will work if and only if it has 3 arguments"
