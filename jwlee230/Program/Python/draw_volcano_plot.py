1f2fd0417e75fd70c2d81344e68cc986fbbeb0e5
Jaewoong Lee <230@fumire.moe>
2021-05-20 14:20:49 +0900

    Separete ADC & SQC

--- jwlee230/Program/Python/draw_volcano_plot.py
+++ jwlee230/Program/Python/draw_volcano_plot.py
@@ -13,8 +13,12 @@ if __name__ == "__main__":
 
     parser.add_argument("DEG", help="DEG TSV file", type=str)
     parser.add_argument("output", help="Output PDF file", type=str)
-    parser.add_argument("--pvalue", help="P-value threshold", type=float, default=0.05)
+    parser.add_argument("--padj", help="P-value threshold", type=float, default=0.05)
     parser.add_argument("--fold", help="Fold change threshold", type=float, default=2)
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--ADC", help="Draw ADC pathway", action="store_true", default=False)
+    group.add_argument("--SQC", help="Draw SQC pathway", action="store_true", default=False)
 
     args = parser.parse_args()
 
@@ -24,30 +28,35 @@ if __name__ == "__main__":
         raise ValueError("Output must end with .PDF!!")
 
     DEG_data = pandas.read_csv(args.DEG, sep="\t", header=0, names=["gene_id", "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj"], index_col="gene_id")
-    DEG_data["-logP"] = -1 * numpy.log10(DEG_data["pvalue"], dtype=float)
+    DEG_data["-log(Padj)"] = -1 * numpy.log10(DEG_data["padj"], dtype=float)
     print(DEG_data)
 
-    up_gene = DEG_data.loc[(DEG_data["log2FoldChange"] >= numpy.log2(args.fold)) & (DEG_data["pvalue"] < args.pvalue), ["log2FoldChange", "-logP"]]
-    down_gene = DEG_data.loc[(DEG_data["log2FoldChange"] <= -1 * numpy.log2(args.fold)) & (DEG_data["pvalue"] < args.pvalue), ["log2FoldChange", "-logP"]]
-    NS_gene = DEG_data.loc[((DEG_data["log2FoldChange"] > -1 * numpy.log2(args.fold)) & (DEG_data["log2FoldChange"] < numpy.log2(args.fold))) | (DEG_data["pvalue"] >= args.pvalue), ["log2FoldChange", "-logP"]]
+    up_gene = DEG_data.loc[(DEG_data["log2FoldChange"] >= numpy.log2(args.fold)) & (DEG_data["padj"] < args.padj), ["log2FoldChange", "-log(Padj)"]]
+    down_gene = DEG_data.loc[(DEG_data["log2FoldChange"] <= -1 * numpy.log2(args.fold)) & (DEG_data["padj"] < args.padj), ["log2FoldChange", "-log(Padj)"]]
+    NS_gene = DEG_data.loc[((DEG_data["log2FoldChange"] > -1 * numpy.log2(args.fold)) & (DEG_data["log2FoldChange"] < numpy.log2(args.fold))) | (DEG_data["padj"] >= args.padj), ["log2FoldChange", "-log(Padj)"]]
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
 
     fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
-    matplotlib.pyplot.scatter(NS_gene["log2FoldChange"], NS_gene["-logP"], c="tab:gray")
-    matplotlib.pyplot.scatter(up_gene["log2FoldChange"], up_gene["-logP"], c="tab:red")
-    matplotlib.pyplot.scatter(down_gene["log2FoldChange"], down_gene["-logP"], c="tab:blue")
-    matplotlib.pyplot.axhline(y=-1 * numpy.log10(args.pvalue), linestyle="--", color="black")
+    matplotlib.pyplot.scatter(NS_gene["log2FoldChange"], NS_gene["-log(Padj)"], c="tab:gray")
+    matplotlib.pyplot.scatter(up_gene["log2FoldChange"], up_gene["-log(Padj)"], c="tab:red")
+    matplotlib.pyplot.scatter(down_gene["log2FoldChange"], down_gene["-log(Padj)"], c="tab:blue")
+    matplotlib.pyplot.axhline(y=-1 * numpy.log10(args.padj), linestyle="--", color="black")
     matplotlib.pyplot.axvline(x=numpy.log2(args.fold), linestyle="--", color="black")
     matplotlib.pyplot.axvline(x=-1 * numpy.log2(args.fold), linestyle="--", color="black")
 
-    matplotlib.pyplot.text(x=0, y=-1 * numpy.log10(args.pvalue), s="p={0:.2f}".format(args.pvalue), horizontalalignment="center", verticalalignment="baseline", fontsize="xx-small")
+    matplotlib.pyplot.text(x=0, y=-1 * numpy.log10(args.padj), s="Padj={0:.2f}".format(args.padj), horizontalalignment="center", verticalalignment="baseline", fontsize="xx-small")
 
     matplotlib.pyplot.grid(True)
     matplotlib.pyplot.xlabel("log2(Fold_Change)")
-    matplotlib.pyplot.ylabel("-log10(P)")
-    matplotlib.pyplot.title("Up: {0:d}, Down: {1:d}".format(len(up_gene), len(down_gene)))
+    matplotlib.pyplot.ylabel("-log10(Padj)")
+    if args.ADC:
+        matplotlib.pyplot.title("Up: {0:d}, Down: {1:d} in ADC".format(len(up_gene), len(down_gene)))
+    elif args.SQC:
+        matplotlib.pyplot.title("Up: {0:d}, Down: {1:d} in SQC".format(len(up_gene), len(down_gene)))
+    else:
+        raise Exception("Something went wrong!!")
 
     fig.savefig(args.output)
     matplotlib.pyplot.close(fig)
