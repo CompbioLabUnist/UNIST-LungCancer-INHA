3e81c9e6a0880708e26e9015bd9370028bfee8c5
Jaewoong Lee <230@fumire.moe>
2021-06-29 12:29:11 +0900

    Add "agg" option

--- jwlee230/Program/Bash/run_pyclone.bash
+++ jwlee230/Program/Bash/run_pyclone.bash
@@ -3,6 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if (($# == 2)); then
+    echo "backend: Agg" > /root/.config/matplotlib/matplotlibrc
     /root/anaconda2/envs/pyclone/bin/PyClone run_analysis_pipeline --in_file $1 --working_dir $2 --seed 42
 else
     echo "This script will work if and only if it has 2 arguments"
