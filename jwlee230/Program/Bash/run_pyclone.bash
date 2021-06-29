69ff8fb635181a29affbbd8b9ffc5b744baf28d1
Jaewoong Lee <230@fumire.moe>
2021-06-29 12:46:33 +0900

    Add mkdir command

--- jwlee230/Program/Bash/run_pyclone.bash
+++ jwlee230/Program/Bash/run_pyclone.bash
@@ -3,6 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if (($# == 2)); then
+    mkdir -p /root/.config/matplotlib
     echo "backend: Agg" > /root/.config/matplotlib/matplotlibrc
     /root/anaconda2/envs/pyclone/bin/PyClone run_analysis_pipeline --in_file $1 --working_dir $2 --seed 42
 else
