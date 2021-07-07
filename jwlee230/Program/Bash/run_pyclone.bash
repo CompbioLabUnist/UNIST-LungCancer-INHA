eb422e26df8d7b6d6c8c5d41082b47f818353784
Jaewoong Lee <230@fumire.moe>
2021-07-07 14:44:35 +0900

    Run PyClone: handle with no intersection error

--- jwlee230/Program/Bash/run_pyclone.bash
+++ jwlee230/Program/Bash/run_pyclone.bash
@@ -2,11 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if (($# == 2)); then
-    mkdir -p /root/.config/matplotlib
-    echo "backend: Agg" > /root/.config/matplotlib/matplotlibrc
-    /root/anaconda2/envs/pyclone/bin/PyClone run_analysis_pipeline --in_file $1 --working_dir $2 --seed 42
+if (($# > 1)); then
+    other=${*:1:$#-1}
+    last=${@:$#}
+    rm -rf $last
+    mkdir $last
+    touch $last/cluster.tsv $last/loci.tsv
+    /root/anaconda2/envs/pyclone/bin/PyClone setup_analysis --in_files $other --working_dir $last --init_method connected
+    /root/anaconda2/envs/pyclone/bin/PyClone run_analysis --config_file $last/config.yaml --seed 42
+    /root/anaconda2/envs/pyclone/bin/PyClone build_table --config_file $last/config.yaml --out_file $last/cluster.tsv --table_type cluster
+    /root/anaconda2/envs/pyclone/bin/PyClone build_table --config_file $last/config.yaml --out_file $last/loci.tsv --table_type loci
 else
-    echo "This script will work if and only if it has 2 arguments"
-    echo "Usage: <SH file> <input file> <working directory>"
+    echo "Usage: <SH file> <input file(s)> <working directory>"
 fi
