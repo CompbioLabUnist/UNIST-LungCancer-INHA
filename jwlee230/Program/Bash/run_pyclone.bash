3e8af3a33e55a1ba7ce39f3c11aab8e3136b65c9
Jaewoong Lee <230@fumire.moe>
2021-07-11 05:47:02 +0900

    Add 'agg' option

--- jwlee230/Program/Bash/run_pyclone.bash
+++ jwlee230/Program/Bash/run_pyclone.bash
@@ -5,13 +5,13 @@ IFS=$'\n\t'
 if (($# > 1)); then
     other=${*:1:$#-1}
     last=${@:$#}
+
     rm -rf $last
-    mkdir $last
+    mkdir -p $last  /root/.config/matplotlib
     touch $last/cluster.tsv $last/loci.tsv
-    /root/anaconda2/envs/pyclone/bin/PyClone setup_analysis --in_files $other --working_dir $last --init_method connected
-    /root/anaconda2/envs/pyclone/bin/PyClone run_analysis --config_file $last/config.yaml --seed 42
-    /root/anaconda2/envs/pyclone/bin/PyClone build_table --config_file $last/config.yaml --out_file $last/cluster.tsv --table_type cluster
-    /root/anaconda2/envs/pyclone/bin/PyClone build_table --config_file $last/config.yaml --out_file $last/loci.tsv --table_type loci
+    echo "backend: Agg" > /root/.config/matplotlib/matplotlibrc
+
+    /root/anaconda2/envs/pyclone/bin/PyClone run_analysis_pipeline --in_files $other --working_dir $last --init_method connected --seed 42
 else
     echo "Usage: <SH file> <input file(s)> <working directory>"
 fi
