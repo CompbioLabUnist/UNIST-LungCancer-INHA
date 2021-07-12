7254465cb3c33e98e430bdb84662da652688cd86
Jaewoong Lee <230@fumire.moe>
2021-07-12 20:38:47 +0900

    Get output from original location

--- jwlee230/Program/Bash/run_pyclone.bash
+++ jwlee230/Program/Bash/run_pyclone.bash
@@ -8,9 +8,11 @@ if (($# > 1)); then
     rm -rf $last
     mkdir -p $last /root/.config/matplotlib
     echo "backend: Agg" > /root/.config/matplotlib/matplotlibrc
-    touch $last/config.yaml
+    touch $last/config.yaml $last/loci.tsv
 
     /root/anaconda2/envs/pyclone/bin/PyClone run_analysis_pipeline --in_files $1 $2 --tumour_contents $(cat $3) $(cat $4) --working_dir $last --init_method connected --seed 42
+
+    ln $last/tables/loci.tsv $last/loci.tsv
 else
     echo "Usage: <SH file> <input file(s)> <working directory>"
 fi
