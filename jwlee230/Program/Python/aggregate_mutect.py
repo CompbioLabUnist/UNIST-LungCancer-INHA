2e886047399169c6a6f0f587f41a2fa93d2e2875
Jaewoong Lee <230@fumire.moe>
2021-07-13 18:53:09 +0900

    Adjust figure sizes

--- jwlee230/Program/Python/aggregate_mutect.py
+++ jwlee230/Program/Python/aggregate_mutect.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     parser.add_argument("mutenricher", help="MutEnricher Fisher enrichment output", type=str)
     parser.add_argument("output", help="Output file", type=str)
     parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
-    parser.add_argument("--gene", help="Gene number to draw", type=int, default=30)
+    parser.add_argument("--gene", help="Gene number to draw", type=int, default=50)
 
     args = parser.parse_args()
 
@@ -71,6 +71,6 @@ if __name__ == "__main__":
     my_comut.add_bar_data(patient_data[["Tumor_Sample_Barcode", "Mutation_Count"]].set_axis(labels=step00.sample_columns, axis="columns"), name="Mutation count", ylabel="Counts", mapping={"group": "purple"})
     my_comut.add_side_bar_data(mutation_data[["Gene", "-log10(P)"]].set_axis(labels=step00.bar_columns, axis="columns"), name="Mutation count", xlabel="-log10(P)", paired_name="Mutation type", position="left", mapping=step00.bar_mapping)
 
-    my_comut.plot_comut(x_padding=0.04, y_padding=0.04, tri_padding=0.03, figsize=(len(args.input), mutation_data.shape[0]))
+    my_comut.plot_comut(x_padding=0.04, y_padding=0.04, tri_padding=0.03, figsize=(len(args.input) * 1.2, mutation_data.shape[0]))
     my_comut.add_unified_legend()
     my_comut.figure.savefig(args.output)
