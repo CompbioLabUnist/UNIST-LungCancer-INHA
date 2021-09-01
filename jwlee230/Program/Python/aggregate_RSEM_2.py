1d0717ebee7520efb5ed496490b9ae7bdd9506e6
Jaewoong Lee <230@fumire.moe>
2021-09-01 13:10:31 +0900

    Refactoring python file

--- jwlee230/Program/Python/aggregate_RSEM_2.py
+++ jwlee230/Program/Python/aggregate_RSEM_2.py
@@ -1,5 +1,5 @@
 """
-aggregate_RSEM2.py: aggregate RSEM results
+aggregate_RSEM_2.py: aggregate RSEM results for recurrence/non-recurrence
 """
 import argparse
 import gtfparse
@@ -33,27 +33,42 @@ def ENSG_to_names(gene_id: str) -> str:
         return result.to_numpy()[0]
 
 
+def recur_or_non(recur, non, ID):
+    if ID in recur:
+        return "Recurrence"
+    elif ID in non:
+        return "Non-recurrence"
+    else:
+        raise Exception("Something went wrong!!")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("input", help="Input genes.results (TSV) files", type=str, nargs="+")
     parser.add_argument("gencode", help="Gencode annotation GTF file", type=str)
     parser.add_argument("trembl", help="Gencode TREMBL gz file", type=str)
-    parser.add_argument("output", help="Output TSV file", type=str)
+    parser.add_argument("output", help="Output file basename", type=str)
+    parser.add_argument("--recur", help="Recurrences RSEM genes.results file(s)", type=str, nargs="+", required=True)
+    parser.add_argument("--non", help="Recurrences RSEM genes.results file(s)", type=str, nargs="+", required=True)
+    parser.add_argument("--target", help="Give compared histologies", required=True)
     parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
 
     args = parser.parse_args()
 
-    if list(filter(lambda x: not x.endswith(".genes.results"), args.input)):
-        raise ValueError("INPUT must end with .genes.results!!")
-    elif not args.gencode.endswith(".gtf"):
+    if not args.gencode.endswith(".gtf"):
         raise ValueError("Gencode must end with .gtf!!")
-    elif not args.output.endswith(".tsv"):
-        raise ValueError("Output must end with .TSV!!")
+    elif list(filter(lambda x: not x.endswith(".genes.results"), args.recur + args.non)):
+        raise ValueError("Recur/Non-recur must end with .genes.results!!")
+    elif args.target not in step00.long_sample_type_list:
+        raise ValueError("TARGET is not valid!!")
     elif args.cpus < 1:
         raise ValueError("CPUs must be positive!!")
 
-    args.input.sort(key=step00.sorting)
+    print(args.recur)
+    args.recur = list(filter(lambda x: step00.get_long_sample_type(x.split("/")[-1].split(".")[0]) == args.target, args.recur))
+    args.non = list(filter(lambda x: step00.get_long_sample_type(x.split("/")[-1].split(".")[0]) == args.target, args.non))
+    input_files = args.recur + args.non
+    print(input_files)
 
     trembl_data = pandas.read_csv(args.trembl, sep="\t")
     trembl_data = trembl_data.loc[(trembl_data["db_name"] == "Uniprot/SPTREMBL"), ["xref", "gene_stable_id"]]
@@ -64,11 +79,12 @@ if __name__ == "__main__":
     print(gencode_data)
 
     with multiprocessing.Pool(args.cpus) as pool:
-        input_data = pandas.concat(objs=pool.map(read_RSEM, args.input), axis="columns", sort=True, verify_integrity=True, copy=False)
+        input_data = pandas.concat(objs=pool.map(read_RSEM, input_files), axis="columns", sort=True, verify_integrity=True, copy=False)
         input_data["ENSG"] = pool.map(trembl_to_ENSG, list(input_data.index))
         input_data = input_data.loc[(input_data["ENSG"] != "")]
         input_data["gene_name"] = pool.map(ENSG_to_names, list(input_data["ENSG"]))
         input_data = input_data.loc[(input_data["gene_name"] != "")]
     print(input_data)
 
-    input_data.groupby(by="gene_name").sum().to_csv(args.output, sep="\t", index=True, header=True)
+    input_data.groupby(by="gene_name").sum().to_csv(args.output + ".tsv", sep="\t", index=True, header=True)
+    pandas.DataFrame(data=[(ID, recur_or_non(args.recur, args.non, ID)) for ID in list(input_data.columns)[:-1]], columns=["ID", "condition"]).to_csv(args.output + ".coldata", sep="\t", index=False, header=True)
