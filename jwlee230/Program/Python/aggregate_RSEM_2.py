b00a5ebe6e71a0f3c66d793efbef3de84b1ffdb1
Jaewoong Lee <230@fumire.moe>
2021-09-06 15:35:46 +0900

    Add comprehensive filtering/comparing options

--- jwlee230/Program/Python/aggregate_RSEM_2.py
+++ jwlee230/Program/Python/aggregate_RSEM_2.py
@@ -2,93 +2,112 @@
 aggregate_RSEM_2.py: aggregate RSEM results for recurrence/non-recurrence
 """
 import argparse
-import gtfparse
-import multiprocessing
+import datetime
 import pandas
 import step00
-
-trembl_data = pandas.DataFrame()
-gencode_data = pandas.DataFrame()
-trembl_ID_set = set()
-
-
-def read_RSEM(filename: str) -> pandas.DataFrame:
-    data = pandas.read_csv(filename, sep="\t", usecols=["gene_id", "TPM"], index_col="gene_id")
-    data.columns = [filename.split("/")[-1].split(".")[0]]
-    return data
-
-
-def trembl_to_ENSG(gene_id: str) -> str:
-    if gene_id in trembl_ID_set:
-        return trembl_data.loc[(trembl_data["xref"] == gene_id), "gene_stable_id"].to_numpy()[0]
-    else:
-        return ""
-
-
-def ENSG_to_names(gene_id: str) -> str:
-    result = gencode_data.loc[(gencode_data["gene_id"].str.contains(gene_id)), "gene_name"]
-    if result.empty:
-        return ""
-    else:
-        return result.to_numpy()[0]
-
-
-def recur_or_non(recur, non, ID):
-    recur = list(map(lambda x: x.split("/")[-1].split(".")[0], recur))
-    non = list(map(lambda x: x.split("/")[-1].split(".")[0], non))
-    if ID in recur:
-        return "Recurrence"
-    elif ID in non:
-        return "Non-recurrence"
-    else:
-        raise Exception("Something went wrong!!")
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("gencode", help="Gencode annotation GTF file", type=str)
-    parser.add_argument("trembl", help="Gencode TREMBL gz file", type=str)
+    parser.add_argument("input", help="Input RSEM filtered TSV file", type=str)
+    parser.add_argument("clinical", help="Clinidata data CSV file", type=str)
     parser.add_argument("output", help="Output file basename", type=str)
-    parser.add_argument("--recur", help="Recurrences RSEM genes.results file(s)", type=str, nargs="+", required=True)
-    parser.add_argument("--non", help="Recurrences RSEM genes.results file(s)", type=str, nargs="+", required=True)
-    parser.add_argument("--target", help="Give compared histologies", required=True)
-    parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
+    parser.add_argument("--compare", help="Comparison grouping (type, control, case)", type=str, nargs=3, default=["stage", "Normal", "Primary"])
+    parser.add_argument("--date", help="Selection is date", action="store_true", default=False)
+
+    group_histology = parser.add_mutually_exclusive_group(required=True)
+    group_histology.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group_histology.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+
+    group_filter = parser.add_mutually_exclusive_group()
+    group_filter.add_argument("--exact", help="Select only exact match (type, value)", type=str, nargs=2)
+    group_filter.add_argument("--range", help="Select only in range (type, min, max)", type=str, nargs=3)
 
     args = parser.parse_args()
 
-    if not args.gencode.endswith(".gtf"):
-        raise ValueError("Gencode must end with .gtf!!")
-    elif list(filter(lambda x: not x.endswith(".genes.results"), args.recur + args.non)):
-        raise ValueError("Recur/Non-recur must end with .genes.results!!")
-    elif args.target not in step00.long_sample_type_list:
-        raise ValueError("TARGET is not valid!!")
-    elif args.cpus < 1:
-        raise ValueError("CPUs must be positive!!")
+    if not args.clinical.endswith(".csv"):
+        raise ValueError("Clinical must end with .CSV!!")
 
-    print(args.recur)
-    args.recur = list(filter(lambda x: step00.get_long_sample_type(x.split("/")[-1].split(".")[0]) == args.target, args.recur))
-    args.non = list(filter(lambda x: step00.get_long_sample_type(x.split("/")[-1].split(".")[0]) == args.target, args.non))
-    input_files = sorted(args.non) + sorted(args.recur)
-    print(input_files)
+    clinical_data = step00.get_clinical_data(args.clinical)
+    clinical_types = sorted(clinical_data.columns)
+    print(clinical_data)
+    print(clinical_types)
 
-    trembl_data = pandas.read_csv(args.trembl, sep="\t")
-    trembl_data = trembl_data.loc[(trembl_data["db_name"] == "Uniprot/SPTREMBL"), ["xref", "gene_stable_id"]]
-    trembl_ID_set = set(trembl_data["xref"])
-    print(trembl_data)
+    if not args.input.endswith(".tsv"):
+        raise ValueError("Input file must end with .TSV!!")
+    elif (args.compare[0] != "stage") and (args.compare[0] not in clinical_types):
+        raise ValueError("Campare should be one of the clinical types or <stage (sample type)>!!")
+    elif (args.exact is not None) and ((args.exact[0] != "stage") and (args.exact[0] not in clinical_types)):
+        raise ValueError("Exact should be one of the clinical types or <stage (sample type)>!!")
+    elif (args.range is not None) and (args.range[0] not in clinical_types):
+        raise ValueError("Range should be one of the clinical types!!")
 
-    gencode_data = gtfparse.read_gtf(args.gencode)[["gene_id", "gene_name"]].drop_duplicates(ignore_index=True)
-    print(gencode_data)
-
-    with multiprocessing.Pool(args.cpus) as pool:
-        input_data = pandas.concat(objs=pool.map(read_RSEM, input_files), axis="columns", sort=True, verify_integrity=True, copy=False)
-        input_data["ENSG"] = pool.map(trembl_to_ENSG, list(input_data.index))
-        input_data = input_data.loc[(input_data["ENSG"] != "")]
-        input_data["gene_name"] = pool.map(ENSG_to_names, list(input_data["ENSG"]))
-        input_data = input_data.loc[(input_data["gene_name"] != "")]
-    del input_data["ENSG"]
+    input_data = pandas.read_csv(args.input, sep="\t", index_col="gene_name")
+    patients = sorted(input_data.columns, key=step00.sorting_by_type)
     print(input_data)
-    print(list(input_data.columns))
+    print(patients)
 
-    input_data.groupby(by="gene_name").sum().to_csv(args.output + ".tsv", sep="\t", index=True, header=True)
-    pandas.DataFrame(data=[(ID, recur_or_non(args.recur, args.non, ID)) for ID in list(input_data.columns)[:-1]], columns=["ID", "condition"]).to_csv(args.output + ".coldata", sep="\t", index=False, header=True)
+    if args.SQC:
+        histology = set(clinical_data.loc[(clinical_data["Histology"] == "SQC")].index)
+        patients = list(filter(lambda x: step00.get_patient(x) in histology, patients))
+    elif args.ADC:
+        histology = set(clinical_data.loc[(clinical_data["Histology"] == "ADC")].index)
+        patients = list(filter(lambda x: step00.get_patient(x) in histology, patients))
+    else:
+        raise Exception("Something went wrong!!")
+    print(patients)
+
+    if args.exact is not None:
+        if args.exact[0] == "stage":
+            assert args.exact[1] in step00.long_sample_type_list, "Invalid stage type!!"
+            patients = list(filter(lambda x: step00.get_long_sample_type(x) == args.exact[1], patients))
+        else:
+            if args.date:
+                value = datetime.datetime.strptime(args.exact[1], "%Y-%m-%d").date()
+            else:
+                value = args.exact[1]
+
+            filtering = set(clinical_data.loc[(clinical_data[args.exact[0]] == value)].index)
+            patients = list(filter(lambda x: step00.get_patient(x) in filtering, patients))
+            assert patients, "Too less patients!!"
+            print(patients)
+
+    if args.range is not None:
+        if args.date:
+            min_value = datetime.datetime.strptime(args.range[1], "%Y-%m-%d").date()
+            max_value = datetime.datetime.strptime(args.range[2], "%Y-%m-%d").date()
+        else:
+            min_value, max_value = args.range[1:]
+
+        filtering = set(clinical_data.loc[(min_value <= clinical_data[args.range[0]]) & (clinical_data[args.range[0]] <= max_value)])
+        patients = list(filter(lambda x: step00.get_patient(x) in filtering, patients))
+        assert patients, "Too less patients!!"
+        print(patients)
+
+    if args.compare[0] == "stage":
+        assert args.compare[1] in step00.long_sample_type_list, "Invalid stage type!!"
+        assert args.compare[2] in step00.long_sample_type_list, "Invalid stage type!!"
+
+        patients = list(filter(lambda x: step00.get_long_sample_type(x) == args.compare[1], patients)) + list(filter(lambda x: step00.get_long_sample_type(x) == args.compare[2], patients))
+        conditions = list(map(step00.get_long_sample_type, patients))
+
+        assert patients, "Too less patients!!"
+        assert len(set(conditions)) == 2, "Invalid conditions!!"
+
+        print(patients)
+        print(conditions)
+    else:
+        f1 = set(clinical_data.loc[(clinical_data[args.compare[1]] == value)].index)
+        f2 = set(clinical_data.loc[(clinical_data[args.compare[2]] == value)].index)
+
+        patients = list(filter(lambda x: step00.get_patient(x) in f1, patients)) + list(filter(lambda x: step00.get_patient(x) in f2, patients))
+        conditions = list(map(lambda x: args.compare[1] if x in f1 else args.compare[2], patients))
+
+        assert patients, "Too less patients!!"
+        assert len(set(conditions)) == 2, "Invalid conditions!!"
+
+        print(patients)
+        print(conditions)
+
+    input_data.loc[:, patients].to_csv(args.output + ".tsv", sep="\t", index=True, header=True)
+    pandas.DataFrame(data=zip(patients, conditions), columns=["ID", "condition"]).to_csv(args.output + ".coldata", sep="\t", index=False, header=True)
