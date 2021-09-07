d1f6b431d683d7ae8228f5aeb75533d12165f500
Jaewoong Lee <230@fumire.moe>
2021-09-07 17:57:54 +0900

    Add bbox for background

--- jwlee230/Program/Python/draw_vaf_plots.py
+++ jwlee230/Program/Python/draw_vaf_plots.py
@@ -47,7 +47,7 @@ def draw_plot(first_sample: str, second_sample: str) -> typing.List[str]:
 
     fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
 
-    texts = []
+    texts = list()
 
     data = merged_data.loc[~(merged_data["gene_census"]) & ~(merged_data["mutation"])]
     matplotlib.pyplot.scatter(data["first_VAF"], data["second_VAF"], c="tab:gray", marker="o", alpha=0.3, s=12 ** 2, edgecolor="none", label="Synonymous mutations")
@@ -59,11 +59,11 @@ def draw_plot(first_sample: str, second_sample: str) -> typing.List[str]:
     matplotlib.pyplot.scatter(data["first_VAF"], data["second_VAF"], c="tab:red", marker="*", alpha=1.0, s=20 ** 2, edgecolor="none", label="Cancer genes")
     for index, d in data.iterrows():
         if (d["first_VAF"] > 0.6) and (d["second_VAF"] == 0.0):
-            texts.append(matplotlib.pyplot.text(d["first_VAF"], d["second_VAF"], "{0}: {1}".format(index[6], index[8]), fontsize="xx-small"))
+            texts.append(matplotlib.pyplot.text(d["first_VAF"], d["second_VAF"], "{0}: {1}".format(index[6], index[8]), fontsize="xx-small", bbox={"color": "white", "alpha": 0.5}))
         elif (d["first_VAF"] == 0.0) and (d["second_VAF"] > 0.6):
-            texts.append(matplotlib.pyplot.text(d["first_VAF"], d["second_VAF"], "{0}: {1}".format(index[6], index[8]), fontsize="xx-small"))
+            texts.append(matplotlib.pyplot.text(d["first_VAF"], d["second_VAF"], "{0}: {1}".format(index[6], index[8]), fontsize="xx-small", bbox={"color": "white", "alpha": 0.5}))
         elif (d["first_VAF"] > 0.0) and (d["second_VAF"] > 0.0):
-            texts.append(matplotlib.pyplot.text(d["first_VAF"], d["second_VAF"], "{0}: {1}".format(index[6], index[8]), fontsize="small"))
+            texts.append(matplotlib.pyplot.text(d["first_VAF"], d["second_VAF"], "{0}: {1}".format(index[6], index[8]), fontsize="small", bbox={"color": "white", "alpha": 0.5}))
 
     matplotlib.pyplot.axline((0, 0), (1, 1), linestyle="--", color="black", alpha=0.3)
     matplotlib.pyplot.grid(True)
@@ -89,18 +89,41 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("input", help="Mutect2 input .MAF files", type=str, nargs="+")
+    parser.add_argument("clinical", help="Clinical data data CSV file", type=str)
     parser.add_argument("gene", help="Cancer gene census CSV file", type=str)
     parser.add_argument("output", help="Output TAR file", type=str)
     parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
+
+    group_histology = parser.add_mutually_exclusive_group(required=True)
+    group_histology.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group_histology.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
 
     args = parser.parse_args()
 
     if list(filter(lambda x: not x.endswith(".maf"), args.input)):
         raise ValueError("INPUT must end with .MAF!!")
+    elif not args.clinical.endswith(".csv"):
+        raise ValueError("Clinical must end with .CSV!!")
     elif not args.output.endswith(".tar"):
         raise ValueError("Output must end with .TAR!!")
     elif args.cpus < 1:
         raise ValueError("CPUs must be positive!!")
+
+    clinical_data = step00.get_clinical_data(args.clinical)
+    print(clinical_data)
+
+    patients = list(map(lambda x: step00.get_patient(x.split("/")[-1].split(".")[0]), args.input))
+
+    if args.SQC:
+        histology = set(clinical_data.loc[(clinical_data["Histology"] == "SQC")].index)
+        patients = list(filter(lambda x: step00.get_patient(x) in histology, patients))
+    elif args.ADC:
+        histology = set(clinical_data.loc[(clinical_data["Histology"] == "ADC")].index)
+        patients = list(filter(lambda x: step00.get_patient(x) in histology, patients))
+    else:
+        raise Exception("Something went wrong!!")
+    print(patients)
+    args.input = list(filter(lambda x: step00.get_patient(x.split("/")[-1].split(".")[0]), args.input))
 
     gene_data = pandas.read_csv(args.gene)
     gene_set = set(gene_data["Gene Symbol"])
@@ -119,7 +142,7 @@ if __name__ == "__main__":
         sample_dict[patient][stage].append(input_file)
     pprint.pprint(sample_dict)
 
-    compare_list = []
+    compare_list = list()
     for patient in sample_dict:
         for first_type, second_type in itertools.combinations(step00.long_sample_type_list, 2):
             if (first_type not in sample_dict[patient]) or (second_type not in sample_dict[patient]):
@@ -129,7 +152,7 @@ if __name__ == "__main__":
     print(len(compare_list))
 
     with multiprocessing.Pool(args.cpus) as pool:
-        files = itertools.chain.from_iterable(pool.starmap(draw_plot, compare_list))
+        files = sorted(itertools.chain.from_iterable(pool.starmap(draw_plot, compare_list)))
 
     with tarfile.open(args.output, "w") as tar:
         for f in sorted(files):
