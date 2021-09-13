45b35c7b50eff77371a3734adc43d5c11c1e261b
Jaewoong Lee <230@fumire.moe>
2021-09-13 12:32:23 +0900

    Handling MuSiC results as figures

--- jwlee230/Program/Python/draw_clustermap_MuSiC.py
+++ jwlee230/Program/Python/draw_clustermap_MuSiC.py
@@ -1,5 +1,5 @@
 """
-draw_clustermap_cibersort.py: draw clustermap plot from CibersortX result
+draw_clustermap_MuSiC.py: draw clustermap plot from MuSiC result
 """
 import argparse
 import matplotlib
@@ -12,7 +12,7 @@ import step00
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("cibersort", help="CIBERSORT result TSV file (not necessarily TSV)", type=str)
+    parser.add_argument("input", help="input result TSV file (not necessarily TSV)", type=str)
     parser.add_argument("output", help="Output PDF file", type=str)
 
     args = parser.parse_args()
@@ -20,14 +20,15 @@ if __name__ == "__main__":
     if not args.output.endswith(".pdf"):
         raise ValueError("Output must end with .PDF!!")
 
-    cibersort_data = pandas.read_csv(args.cibersort, sep="\t", index_col="Mixture").T
-    print(cibersort_data)
+    input_data = pandas.read_csv(args.input, sep="\t")
+    input_data = input_data.set_index(list(input_data.columns)[0]).T
+    print(input_data)
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
     seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
 
-    g = seaborn.clustermap(data=cibersort_data, figsize=(max(cibersort_data.shape), cibersort_data.shape[0]), row_cluster=True, col_cluster=True, cbar_pos=None, col_colors=list(map(step00.get_color_by_type, cibersort_data.columns)), tree_kws={"linewidths": 2.0}, xticklabels=True, yticklabels=True, square=False, cmap="Reds")
+    g = seaborn.clustermap(data=input_data, figsize=(max(input_data.shape), input_data.shape[0]), row_cluster=True, col_cluster=True, cbar_pos=None, col_colors=list(map(step00.get_color_by_type, input_data.columns)), tree_kws={"linewidths": 2.0}, xticklabels=True, yticklabels=True, square=False, cmap="Reds")
     g.ax_heatmap.set_xlabel("")
 
     g.savefig(args.output)
