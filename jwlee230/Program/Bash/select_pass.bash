a100a1d07731002d87d63e9b5620a5bdd47dc4f0
Jaewoong Lee <230@fumire.moe>
2021-10-19 17:00:11 +0900

    Use long option instead

--- jwlee230/Program/Bash/select_pass.bash
+++ jwlee230/Program/Bash/select_pass.bash
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if (($# == 2)); then
-    awk -F '\t' '{if($0 ~ /\#/) print; else if($7 == "PASS") print}' $1 > $2
+    awk --field-separator='\t' '{if($0 ~ /\#/) print; else if($7 == "PASS") print}' $1 > $2
 else
     echo "This script will work if and only if it has 2 arguments"
 fi
