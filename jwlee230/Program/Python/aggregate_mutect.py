77c92dc326bdbd546fb74f6c68b7464bea724598
Jaewoong Lee <230@fumire.moe>
2021-10-28 19:10:45 +0900

    Some of issue #11 1. Compare CGC & non-CGC 2. Add gene present rate

--- jwlee230/Program/Python/aggregate_mutect.py
+++ jwlee230/Program/Python/aggregate_mutect.py
@@ -26,9 +26,13 @@ if __name__ == "__main__":
     parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
     parser.add_argument("--p", help="P-value threshold", type=float, default=0.05)
 
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
-    group.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+    group_subtype = parser.add_mutually_exclusive_group(required=True)
+    group_subtype.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group_subtype.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+
+    group_including = parser.add_mutually_exclusive_group(required=True)
+    group_including.add_argument("--include", help="Use included gene with CGC", action="store_true", default=False)
+    group_including.add_argument("--exclude", help="Use included gene with CGC", action="store_true", default=False)
 
     args = parser.parse_args()
 
@@ -61,15 +65,12 @@ if __name__ == "__main__":
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
 
-    my_comut = comut.CoMut()
-    my_comut.samples = sorted(list(map(lambda x: x.split("/")[-1].split(".")[0], args.input)), key=step00.sorting)
-
     with multiprocessing.Pool(args.cpus) as pool:
         mutect_data = pandas.concat(pool.map(read_maf, args.input), ignore_index=True, copy=False)
 
     mutect_data = mutect_data.loc[(mutect_data["Variant_Classification"].isin(step00.nonsynonymous_mutations))]
     mutect_data["Tumor_Sample_Barcode"] = list(map(lambda x: x.split(".")[0], mutect_data["Tumor_Sample_Barcode"]))
-    mutect_data["Variant_Classification"] = list(map(lambda x: {"Nonsense_Mutation": "Nonsense", "In_Frame_Del": "In frame indel", "In_Frame_Ins": "In frame indel", "Frame_Shift_Del": "Frameshift indel", "Missense_Mutation": "Missense", "Splice_Site": "Splice site", "Frame_Shift_Ins": "Frameshift indel", "Translation_Start_Site": "TSS", "Nonstop_Mutation": "Nonstop"}[x], mutect_data["Variant_Classification"]))
+    mutect_data["Variant_Classification"] = list(map(lambda x: step00.nonsynonymous_notations[x], mutect_data["Variant_Classification"]))
     print(mutect_data)
 
     counter: collections.Counter = collections.Counter(mutect_data["Hugo_Symbol"])
@@ -78,27 +79,36 @@ if __name__ == "__main__":
     print(census_data)
 
     driver_data = pandas.read_csv(args.driver, sep="\t")
-    driver_data = driver_data.loc[(driver_data["Gene"].isin(mutect_data["Hugo_Symbol"])) & (driver_data["Gene"].isin(census_data["Gene Symbol"]))]
+    if args.include:
+        driver_data = driver_data.loc[(driver_data["Gene"].isin(mutect_data["Hugo_Symbol"])) & (driver_data["Gene"].isin(census_data["Gene Symbol"]))]
+    elif args.exclude:
+        driver_data = driver_data.loc[(driver_data["Gene"].isin(mutect_data["Hugo_Symbol"])) & ~(driver_data["Gene"].isin(census_data["Gene Symbol"]))]
+    else:
+        raise Exception("Something went wrong!!")
     for column in step00.MutEnricher_pval_columns:
         driver_data = driver_data.loc[(driver_data[column] < args.p)]
     driver_data.sort_values(by="Fisher_pval", ascending=False, ignore_index=True, inplace=True)
-    driver_data["Count"] = list(map(lambda x: counter[x], driver_data["Gene"]))
+    driver_data["Rate"] = list(map(lambda x: counter[x] / len(args.input), driver_data["Gene"]))
     driver_data["-log10(P)"] = -1 * numpy.log10(driver_data["Fisher_pval"])
     print(driver_data)
+
+    my_comut = comut.CoMut()
+    my_comut.samples = sorted(list(map(lambda x: x.split("/")[-1].split(".")[0], args.input)), key=step00.sorting)
 
     patient_data = pandas.DataFrame()
     patient_data["Tumor_Sample_Barcode"] = my_comut.samples
     patient_data["Patient"] = list(map(lambda x: hash(step00.get_patient(x)), my_comut.samples))
     patient_data["Collection_Type_category"] = "Type"
     patient_data["Collection_Type_value"] = list(map(step00.get_long_sample_type, my_comut.samples))
-    patient_data["Mutation_Count"] = list(map(lambda x: mutect_data.loc[(mutect_data["Tumor_Sample_Barcode"] == x)].shape[0], my_comut.samples))
+    patient_data["non-synonymous"] = list(map(lambda x: mutect_data.loc[(mutect_data["Tumor_Sample_Barcode"] == x)].shape[0], my_comut.samples))
     print(patient_data)
 
     my_comut.add_sample_indicators(patient_data[["Tumor_Sample_Barcode", "Patient"]].set_axis(labels=["sample", "group"], axis="columns"), name="Same patient")
     my_comut.add_categorical_data(patient_data[["Tumor_Sample_Barcode", "Collection_Type_category", "Collection_Type_value"]].set_axis(labels=["sample", "category", "value"], axis="columns"), name="Type", value_order=step00.long_sample_type_list)
-    my_comut.add_categorical_data(mutect_data[["Tumor_Sample_Barcode", "Hugo_Symbol", "Variant_Classification"]].set_axis(labels=["sample", "category", "value"], axis="columns"), name="Mutation type", category_order=driver_data["Gene"], priority=["Frameshift indel"], mapping={"Missense": "darkgreen", "Nonsense": "deeppink", "In frame indel": "navy", "Frameshift indel": "gold", "Splice site": "darkviolet", "LOH": "orange", "TSS": "chocolate", "Nonstop": "crimson", "Absent": "lightgray", "Multiple": "black"})
-    my_comut.add_bar_data(patient_data[["Tumor_Sample_Barcode", "Mutation_Count"]].set_axis(labels=["sample", "group"], axis="columns"), name="Mutation count", ylabel="Counts", mapping={"group": "purple"})
-    my_comut.add_side_bar_data(driver_data[["Gene", "-log10(P)"]].set_axis(labels=["category", "value"], axis="columns"), name="Mutation count", xlabel="-log10(P)", paired_name="Mutation type", position="left", mapping={"value": "olive"})
+    my_comut.add_categorical_data(mutect_data[["Tumor_Sample_Barcode", "Hugo_Symbol", "Variant_Classification"]].set_axis(labels=["sample", "category", "value"], axis="columns"), name="Mutation type", category_order=driver_data["Gene"], priority=["Frameshift indel"], mapping=step00.nonsynonymous_coloring)
+    my_comut.add_bar_data(patient_data[["Tumor_Sample_Barcode", "non-synonymous"]].set_axis(labels=["sample", "Counts"], axis="columns"), name="Mutation count", ylabel="Mutations", mapping={"Counts": "purple"})
+    my_comut.add_side_bar_data(driver_data[["Gene", "-log10(P)"]].set_axis(labels=["category", "value"], axis="columns"), name="P-value", xlabel="-log10(P)", paired_name="Mutation type", position="left", mapping={"value": "olive"})
+    my_comut.add_side_bar_data(driver_data[["Gene", "Rate"]].set_axis(labels=["category", "value"], axis="columns"), name="Mutation rate", xlabel="Present Rate", paired_name="Mutation type", position="right", mapping={"value": "teal"})
 
     my_comut.plot_comut(x_padding=0.04, y_padding=0.04, tri_padding=0.03, figsize=(len(args.input), driver_data.shape[0] * 2))
     my_comut.add_unified_legend()
