8438dbf545001f75be231d6c4941550e2f870b01
Jaewoong Lee <230@fumire.moe>
2021-11-08 18:53:35 +0900

    Upon the issue #18 1. Only for subtype 2. With clinical data 3. Add them as recipe

---
+++ jwlee230/Program/Python/plot_signature.py
@@ -0,0 +1,97 @@
+"""
+plot_signature.py: plot cancer signature by stage
+"""
+import argparse
+import itertools
+import multiprocessing
+import os.path
+import tarfile
+import typing
+import matplotlib
+import matplotlib.pyplot
+import pandas
+import scipy.stats
+import seaborn
+import statannotations.Annotator
+import tqdm
+import step00
+
+input_data = pandas.DataFrame()
+order: typing.List[str] = list()
+
+
+def draw_violin(signature: str) -> pandas.DataFrame:
+    fig, ax = matplotlib.pyplot.subplots(figsize=(7 * len(order), 24))
+
+    seaborn.violinplot(data=input_data, x="Subtype", y=signature, order=order, inner="box", ax=ax)
+    statannotations.Annotator.Annotator(ax, list(itertools.combinations(order, 2)), data=input_data, x="Subtype", y=signature, order=order).configure(test="Mann-Whitney", text_format="star", loc="inside", verbose=0).apply_and_annotate()
+
+    matplotlib.pyplot.ylabel("Score")
+    matplotlib.pyplot.title(signature)
+
+    fig_name = "{0}.pdf".format(signature)
+    fig.savefig(os.path.join(step00.tmpfs, fig_name))
+    matplotlib.pyplot.close(fig)
+
+    outputs = [signature]
+    for control, case in list(itertools.combinations(order, 2)):
+        outputs.append(scipy.stats.mannwhitneyu(list(input_data.loc[(input_data["Subtype"] == control), signature]), list(input_data.loc[(input_data["Subtype"] == case), signature]))[1])
+
+    return pandas.DataFrame(data=outputs, index=["Signature"] + list(map(lambda x: x[0] + "-" + x[1], list(itertools.combinations(order, 2))))).T
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Signature TSV file (not necessarily TSV)", type=str)
+    parser.add_argument("clinical", help="Clinidata data CSV file", type=str)
+    parser.add_argument("output", help="Output TAR file", type=str)
+    parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
+
+    group_subtype = parser.add_mutually_exclusive_group(required=True)
+    group_subtype.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group_subtype.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if not args.clinical.endswith(".csv"):
+        raise ValueError("Clinical data must end with .csv!!")
+    elif not args.output.endswith(".tar"):
+        raise ValueError("Output must end with .TAR!!")
+    elif args.cpus < 1:
+        raise ValueError("CPUs must be positive!!")
+
+    matplotlib.use("Agg")
+    matplotlib.rcParams.update(step00.matplotlib_parameters)
+    seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
+
+    input_data = pandas.read_csv(args.input, sep="\t", index_col="Samples")
+    signatures = list(input_data.columns)
+    print(input_data)
+
+    clinical_data = step00.get_clinical_data(args.clinical)
+    print(clinical_data)
+
+    if args.SQC:
+        patients = set(clinical_data.loc[(clinical_data["Histology"] == "SQC")].index)
+    elif args.ADC:
+        patients = set(clinical_data.loc[(clinical_data["Histology"] == "ADC")].index)
+    else:
+        raise Exception("Something went wrong!!")
+    print(sorted(patients))
+
+    input_data = input_data.loc[sorted(filter(lambda x: step00.get_patient(x) in patients, list(input_data.index)), key=step00.sorting_by_type), :]
+    input_data["Subtype"] = list(map(step00.get_long_sample_type, list(input_data.index)))
+    order = list(filter(lambda x: x in set(input_data["Subtype"]), step00.long_sample_type_list))
+    print(input_data)
+    print(order)
+
+    with multiprocessing.Pool(args.cpus) as pool:
+        output_data = pandas.concat(objs=pool.map(draw_violin, signatures), join="outer", ignore_index=True, axis="index").set_index(keys="Signature", verify_integrity=True)
+    output_data.to_csv(os.path.join(step00.tmpfs, "output.tsv"), sep="\t", float_format="{:.2e}".format)
+    print(output_data)
+
+    with tarfile.open(name=args.output, mode="w") as tar:
+        tar.add(os.path.join(step00.tmpfs, "output.tsv"), arcname="output.tsv")
+        for file in tqdm.tqdm(list(output_data.index)):
+            tar.add(os.path.join(step00.tmpfs, file + ".pdf"), arcname=file + ".pdf")
---
+++ jwlee230/Program/Python/plot_signature_2.py
@@ -0,0 +1,109 @@
+"""
+plot_signature_2.py: plot cancer signature by stage with clinical data
+"""
+import argparse
+import multiprocessing
+import os.path
+import tarfile
+import typing
+import matplotlib
+import matplotlib.pyplot
+import pandas
+import scipy.stats
+import seaborn
+import statannotations.Annotator
+import tqdm
+import step00
+
+input_data = pandas.DataFrame()
+order: typing.List[str] = list()
+compare_order: typing.List[str] = list()
+hue_order: typing.List[str] = list()
+
+
+def draw_violin(signature: str, clinical: str) -> pandas.DataFrame:
+    fig, ax = matplotlib.pyplot.subplots(figsize=(7 * len(order), 24))
+
+    seaborn.violinplot(data=input_data, x="Subtype", y=signature, order=order, hue=clinical, hue_order=hue_order, inner="box", ax=ax)
+    statannotations.Annotator.Annotator(ax, list(map(lambda x: [(x, hue_order[0]), (x, hue_order[1])], compare_order)), data=input_data, x="Subtype", y=signature, order=order, hue=clinical, hue_order=hue_order).configure(test="Mann-Whitney", text_format="star", loc="inside", verbose=0).apply_and_annotate()
+
+    matplotlib.pyplot.ylabel("Score")
+    matplotlib.pyplot.title(signature)
+
+    fig_name = "{0}.pdf".format(signature)
+    fig.savefig(os.path.join(step00.tmpfs, fig_name))
+    matplotlib.pyplot.close(fig)
+
+    outputs = [signature]
+    for subtype in compare_order:
+        outputs.append(scipy.stats.mannwhitneyu(list(input_data.loc[(input_data["Subtype"] == subtype) & (input_data[clinical] == hue_order[0]), signature]), list(input_data.loc[(input_data["Subtype"] == subtype) & (input_data[clinical] == hue_order[1]), signature]))[1])
+
+    return pandas.DataFrame(data=outputs, index=["Signature"] + compare_order).T
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Signature TSV file (not necessarily TSV)", type=str)
+    parser.add_argument("clinical", help="Clinidata data CSV file", type=str)
+    parser.add_argument("output", help="Output TAR file", type=str)
+    parser.add_argument("--compare", help="Comparison grouping (type, control, case)", type=str, nargs=3, default=["Recurrence", "NO", "YES"])
+    parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
+
+    group_subtype = parser.add_mutually_exclusive_group(required=True)
+    group_subtype.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group_subtype.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if not args.clinical.endswith(".csv"):
+        raise ValueError("Clinical data must end with .csv!!")
+    elif not args.output.endswith(".tar"):
+        raise ValueError("Output must end with .TAR!!")
+    elif args.cpus < 1:
+        raise ValueError("CPUs must be positive!!")
+
+    matplotlib.use("Agg")
+    matplotlib.rcParams.update(step00.matplotlib_parameters)
+    seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
+
+    input_data = pandas.read_csv(args.input, sep="\t", index_col="Samples")
+    signatures = list(input_data.columns)
+    print(input_data)
+
+    clinical_data = step00.get_clinical_data(args.clinical)
+    print(clinical_data)
+
+    if args.SQC:
+        control_patients = set(clinical_data.loc[(clinical_data["Histology"] == "SQC") & (clinical_data[args.compare[0]] == args.compare[1])].index)
+        case_patients = set(clinical_data.loc[(clinical_data["Histology"] == "SQC") & (clinical_data[args.compare[0]] == args.compare[2])].index)
+    elif args.ADC:
+        control_patients = set(clinical_data.loc[(clinical_data["Histology"] == "ADC") & (clinical_data[args.compare[0]] == args.compare[1])].index)
+        case_patients = set(clinical_data.loc[(clinical_data["Histology"] == "ADC") & (clinical_data[args.compare[0]] == args.compare[2])].index)
+    else:
+        raise Exception("Something went wrong!!")
+    patients = set(list(control_patients) + list(case_patients))
+    print(sorted(control_patients))
+    print(sorted(case_patients))
+
+    input_data = input_data.loc[sorted(filter(lambda x: step00.get_patient(x) in patients, list(input_data.index)), key=step00.sorting_by_type), :]
+    input_data["Subtype"] = list(map(step00.get_long_sample_type, list(input_data.index)))
+    input_data[args.compare[0]] = list(map(lambda x: args.compare[1] if (step00.get_patient(x) in control_patients) else args.compare[2], list(input_data.index)))
+    print(input_data)
+
+    order = list(filter(lambda x: x in set(input_data["Subtype"]), step00.long_sample_type_list))
+    compare_order = list(filter(lambda x: not input_data.loc[(input_data["Subtype"] == x) & (input_data[args.compare[0]] == args.compare[1])].empty and not input_data.loc[(input_data["Subtype"] == x) & (input_data[args.compare[0]] == args.compare[2])].empty, order))
+    hue_order = args.compare[1:]
+    print(order)
+    print(compare_order)
+    print(hue_order)
+
+    with multiprocessing.Pool(args.cpus) as pool:
+        output_data = pandas.concat(objs=pool.starmap(draw_violin, [(signature, args.compare[0]) for signature in signatures]), join="outer", ignore_index=True, axis="index").set_index(keys="Signature", verify_integrity=True)
+    output_data.to_csv(os.path.join(step00.tmpfs, "output.tsv"), sep="\t", float_format="{:.2e}".format)
+    print(output_data)
+
+    with tarfile.open(name=args.output, mode="w") as tar:
+        tar.add(os.path.join(step00.tmpfs, "output.tsv"), arcname="output.tsv")
+        for file in tqdm.tqdm(list(output_data.index)):
+            tar.add(os.path.join(step00.tmpfs, file + ".pdf"), arcname=file + ".pdf")
