11f0657a2e2613949348c0c7d54004731fd2f991
Jaewoong Lee <230@fumire.moe>
2021-12-07 12:53:25 +0900

    Update FastQC aggregation

--- jwlee230/Program/Python/aggregate_fastqc.py
+++ jwlee230/Program/Python/aggregate_fastqc.py
@@ -7,6 +7,7 @@ import zipfile
 import matplotlib
 import matplotlib.pyplot
 import pandas
+import tqdm
 import step00
 
 if __name__ == "__main__":
@@ -25,37 +26,41 @@ if __name__ == "__main__":
     elif args.threshold < 0:
         raise ValueError("Threshold must be positive!!")
 
-    args.input.sort()
+    args.input.sort(key=step00.sorting)
 
-    for file_name in args.input:
+    for file_name in tqdm.tqdm(args.input):
         with zipfile.ZipFile(file_name) as zip_file:
             zip_file.extract(list(filter(lambda x: x.endswith("summary.txt"), zip_file.namelist()))[0], path=step00.tmpfs)
 
     raw_data = list()
-    for directory in step00.directory_list(step00.tmpfs):
+    for directory in tqdm.tqdm(step00.directory_list(step00.tmpfs)):
         with open(os.path.join(directory, "summary.txt"), "r") as f:
             for line in f.readlines():
                 split_line = line.strip().split("\t")
                 raw_data.append(split_line)
 
     data = pandas.DataFrame(raw_data, columns=["Result", "Item", "File"])
+    print(data)
+
+    file_set = set(data["File"])
+    item_set = set(data["Item"]) - {"Per tile sequence quality", "Per sequence GC content", "Adapter Content"}
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
 
-    fig, ax = matplotlib.pyplot.subplots(figsize=(64, 18))
+    fig, ax = matplotlib.pyplot.subplots(figsize=(len(file_set) / 5, len(item_set) * 2))
 
-    for i, f in enumerate(sorted(set(data["File"]))):
+    for i, f in tqdm.tqdm(enumerate(sorted(file_set))):
         FAIL_count = 0
-        for j, item in enumerate(sorted(set(data["Item"]))):
+        for j, item in enumerate(sorted(item_set, reverse=True)):
             result = data.loc[(data["Item"] == item) & (data["File"] == f), "Result"].to_numpy()[0]
 
             if result == "PASS":
-                matplotlib.pyplot.scatter(i, j, s=30, marker="o", c="tab:green")
+                matplotlib.pyplot.scatter(i, j, s=100, marker="o", c="tab:green")
             elif result == "WARN":
-                matplotlib.pyplot.scatter(i, j, s=30, marker="^", c="tab:olive")
+                matplotlib.pyplot.scatter(i, j, s=100, marker="^", c="tab:olive")
             elif result == "FAIL":
-                matplotlib.pyplot.scatter(i, j, s=30, marker="X", c="tab:red")
+                matplotlib.pyplot.scatter(i, j, s=100, marker="X", c="tab:red")
                 FAIL_count += 1
             else:
                 raise Exception("Something went wrong!!")
@@ -63,9 +68,10 @@ if __name__ == "__main__":
             print(f, FAIL_count)
 
     matplotlib.pyplot.xticks([])
-    matplotlib.pyplot.yticks(range(len(set(data["Item"]))), sorted(set(data["Item"])), fontsize="xx-small")
+    matplotlib.pyplot.yticks(range(len(item_set)), sorted(item_set, reverse=True), fontsize="xx-small")
     matplotlib.pyplot.xlabel("Files")
     matplotlib.pyplot.ylabel("Tests")
+    matplotlib.pyplot.tight_layout()
 
     fig.savefig(args.output)
     matplotlib.pyplot.close(fig)
