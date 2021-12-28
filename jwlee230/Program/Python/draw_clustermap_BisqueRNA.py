f810e9ccfa6f5de5ce07f9c9bb1becde83fbf903
Jaewoong Lee <230@fumire.moe>
2021-12-28 15:32:08 +0900

    Update cluster map (#12) 1. Unify color scheme 2. Add color bar

--- jwlee230/Program/Python/draw_clustermap_BisqueRNA.py
+++ jwlee230/Program/Python/draw_clustermap_BisqueRNA.py
@@ -3,6 +3,7 @@ draw_clustermap_BisqueRNA.py: draw clustermap plot from BisqueRNA result
 """
 import argparse
 import matplotlib
+import matplotlib.patches
 import matplotlib.pyplot
 import pandas
 import seaborn
@@ -30,8 +31,16 @@ if __name__ == "__main__":
     matplotlib.rcParams.update(step00.matplotlib_parameters)
     seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
 
-    g = seaborn.clustermap(data=input_data, figsize=(max(input_data.shape), input_data.shape[0]), row_cluster=True, col_cluster=True, cbar_pos=None, col_colors=list(map(step00.get_color_by_type, input_data.columns)), tree_kws={"linewidths": 2.0}, xticklabels=True, yticklabels=True, square=False, cmap="Reds", vmin=0, vmax=1)
+    palette = list(map(lambda x: step00.stage_color_code[step00.get_long_sample_type(x)], list(input_data.columns)))
+    stage_set = set(map(step00.get_long_sample_type, list(input_data.columns)))
+    stage_list = list(filter(lambda x: x in stage_set, step00.long_sample_type_list))
+
+    g = seaborn.clustermap(data=input_data, figsize=(max(input_data.shape), input_data.shape[0]), row_cluster=True, col_cluster=True, cbar_pos=(-0.04, 0.2, 0.02, 0.6), col_colors=palette, tree_kws={"linewidths": 2.0}, xticklabels=True, yticklabels=True, square=False, cmap="Reds", vmin=0, vmax=1)
+
     g.ax_heatmap.set_xlabel("")
     g.ax_heatmap.set_ylabel("")
+
+    matplotlib.pyplot.legend([matplotlib.patches.Patch(facecolor=step00.stage_color_code[x]) for x in stage_list], stage_list, title="Stages", bbox_to_anchor=(0, 1), bbox_transform=matplotlib.pyplot.gcf().transFigure)
+    matplotlib.pyplot.tight_layout()
 
     g.savefig(args.output)
