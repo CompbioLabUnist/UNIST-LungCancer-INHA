3ec1b8a0c0c2fb4b044294210c394c09360d59c8
Jaewoong Lee <230@fumire.moe>
2022-01-27 18:59:22 +0900

    Give to TEX output (#27)

--- jwlee230/Program/Python/draw_venn_plot_gistic.py
+++ jwlee230/Program/Python/draw_venn_plot_gistic.py
@@ -2,7 +2,7 @@
 draw_venn_plot_gistic.py: draw venn diagram plot with gistic peak
 """
 import argparse
-import pprint
+import re
 import matplotlib
 import matplotlib.pyplot
 import pandas
@@ -16,11 +16,24 @@ def generate_logics(n_sets):
         yield bin(i)[2:].zfill(n_sets)
 
 
+def sorting_cytoband(s):
+    s_split = list(map(int, re.split(r"p|q|\.", s)))
+
+    if "p" in s:
+        s_split.insert(1, "p")
+    elif "q" in s:
+        s_split.insert(1, "q")
+    else:
+        raise Exception("Something went wrong!!")
+
+    return s_split
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("input", help="Input Gistic all_lesions.conf_99.txt file(s)", type=str, nargs="+")
-    parser.add_argument("output", help="Output PDF file", type=str)
+    parser.add_argument("output", help="Output file basename", type=str)
     parser.add_argument("--annotation", help="Annotation for venn diagram", type=str, nargs="+", required=True)
 
     group = parser.add_mutually_exclusive_group(required=True)
@@ -31,10 +44,8 @@ if __name__ == "__main__":
 
     if list(filter(lambda x: not x.endswith("/all_lesions.conf_99.txt"), args.input)):
         raise ValueError("Input is not valid!!")
-    if len(args.input) != len(args.annotation):
+    elif len(args.input) != len(args.annotation):
         raise ValueError("Annotation must be one-to-one upon DEG!!")
-    elif not args.output.endswith(".pdf"):
-        raise ValueError("Output must end with .PDF!!")
 
     input_data = dict()
     for annotation, input_file in tqdm.tqdm(list(zip(args.annotation, args.input))):
@@ -47,7 +58,21 @@ if __name__ == "__main__":
         else:
             raise Exception("Something went wrong!!")
         input_data[annotation] = set(filter(lambda x: (not x.startswith("X")) and (not x.startswith("Y")), input_data[annotation]))
-    pprint.pprint(input_data)
+    print(input_data)
+
+    every_cytoband = sorted(set.union(*list(input_data.values())), key=sorting_cytoband)
+    print(every_cytoband)
+
+    if every_cytoband:
+        output_data = pandas.DataFrame(data=[["" for x in args.annotation] for y in every_cytoband], index=every_cytoband, columns=args.annotation, dtype=str)
+        for annotation in tqdm.tqdm(args.annotation):
+            output_data.loc[input_data[annotation], annotation] = "*"
+    else:
+        output_data = pandas.DataFrame(data=[["" for x in args.annotation]], index=[""], columns=args.annotation, dtype=str)
+
+    print(output_data)
+
+    output_data.to_latex(args.output + ".tex", column_format="l" + "c" * len(args.annotation))
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
@@ -60,18 +85,15 @@ if __name__ == "__main__":
         pass
     matplotlib.pyplot.tight_layout()
 
-    fig.savefig(args.output)
+    fig.savefig(args.output + ".pdf")
     matplotlib.pyplot.close(fig)
 
     datasets = list(input_data.values())
-    n_sets = len(datasets)
-    dataset_union = set.union(*datasets)
-    petal_labels = dict()
+    dataset_union = set(every_cytoband)
+    for logic in generate_logics(len(args.annotation)):
+        included_sets = [datasets[i] for i in range(len(args.annotation)) if logic[i] == "1"]
+        excluded_sets = [datasets[i] for i in range(len(args.annotation)) if logic[i] == "0"]
 
-    for logic in generate_logics(n_sets):
-        included_sets = [datasets[i] for i in range(n_sets) if logic[i] == "1"]
-        excluded_sets = [datasets[i] for i in range(n_sets) if logic[i] == "0"]
-        petal_labels[logic] = sorted((dataset_union & set.intersection(*included_sets)) - set.union(set(), *excluded_sets))
-
-    print(list(input_data.keys()))
-    pprint.pprint(petal_labels)
+        print("+".join(list(map(lambda y: y[0], list(filter(lambda x: x[1] == "1", zip(args.annotation, list(logic))))))))
+        print(sorted((dataset_union & set.intersection(*included_sets)) - set.union(set(), *excluded_sets), key=sorting_cytoband))
+        print()
