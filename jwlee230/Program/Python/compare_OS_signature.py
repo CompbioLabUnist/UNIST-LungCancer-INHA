51120f0737b45962bf1de83d147470186702467a
Jaewoong Lee <230@fumire.moe>
2022-02-23 19:25:02 +0900

    Upon the issue #18 1. VDT 2. RFS 3. OS

--- jwlee230/Program/Python/compare_OS_signature.py
+++ jwlee230/Program/Python/compare_OS_signature.py
@@ -1,10 +1,9 @@
 """
-compare_mutation_signature.py: compare mutation intersection proportion vs. mutation signature
+compare_OS_signature.py: compare OS (Overall Survival) vs. mutational signature
 """
 import argparse
 import itertools
 import tarfile
-import multiprocessing
 import matplotlib
 import matplotlib.pyplot
 import scipy.stats
@@ -13,19 +12,12 @@ import pandas
 import tqdm
 import step00
 
-
-def read_maf(filename: str) -> pandas.DataFrame:
-    return pandas.read_csv(filename, sep="\t", comment="#", low_memory=False)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("input", help="Mutect2 input .MAF files", type=str, nargs="+")
     parser.add_argument("signature", help="Mutation signature TSV file (not necessarily TSV)", type=str)
     parser.add_argument("clinical", help="Clinidata data CSV file", type=str)
     parser.add_argument("output", help="Output TAR file", type=str)
-    parser.add_argument("--cpus", help="CPUs to use", type=int, default=1)
 
     group_subtype = parser.add_mutually_exclusive_group(required=True)
     group_subtype.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
@@ -33,16 +25,13 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if list(filter(lambda x: not x.endswith(".maf"), args.input)):
-        raise ValueError("INPUT must end with .MAF!!")
-    elif not args.clinical.endswith(".csv"):
+    if not args.clinical.endswith(".csv"):
         raise ValueError("Clinical must end with .CSV!!")
     elif not args.output.endswith(".tar"):
         raise ValueError("Output must end with .TAR!!")
-    elif args.cpus < 1:
-        raise ValueError("CPUs must be positive!!")
 
     clinical_data = step00.get_clinical_data(args.clinical)
+    clinical_data = clinical_data.loc[~(clinical_data["Overall Survival"].isna())]
     print(clinical_data)
 
     if args.SQC:
@@ -53,23 +42,14 @@ if __name__ == "__main__":
         raise Exception("Something went wrong!!")
     print(sorted(patients))
 
-    args.input = list(filter(lambda x: step00.get_patient(x) in patients, args.input))
-    sample_list = list(map(step00.get_id, args.input))
-    precancer_sample_list = list(filter(lambda x: step00.get_long_sample_type(x) != "Primary", sample_list))
-    print(sample_list)
-
-    with multiprocessing.Pool(args.cpus) as pool:
-        mutect_data = pandas.concat(pool.map(read_maf, args.input), ignore_index=True, copy=False)
-        mutect_data = mutect_data.loc[(mutect_data["Variant_Classification"].isin(step00.nonsynonymous_mutations))]
-        mutect_data["Tumor_Sample_Barcode"] = list(map(lambda x: x.split(".")[0], mutect_data["Tumor_Sample_Barcode"]))
-        mutect_data["Stage"] = pool.map(step00.get_long_sample_type, mutect_data["Tumor_Sample_Barcode"])
-        mutect_data["Patient"] = pool.map(step00.get_patient, mutect_data["Tumor_Sample_Barcode"])
-    print(mutect_data)
-
     signature_data = pandas.read_csv(args.signature, sep="\t", index_col="Samples")
     for index in tqdm.tqdm(list(signature_data.index)):
         signature_data.loc[index, :] = signature_data.loc[index, :] / sum(signature_data.loc[index, :])
     print(signature_data)
+
+    sample_list = list(filter(lambda x: step00.get_patient(x) in patients, list(signature_data.index)))
+    precancer_sample_list = list(filter(lambda x: step00.get_long_sample_type(x) != "Primary", sample_list))
+    print(sample_list)
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
@@ -77,13 +57,7 @@ if __name__ == "__main__":
 
     output_data = pandas.DataFrame(index=precancer_sample_list)
     output_data["Stage"] = list(map(step00.get_long_sample_type, list(output_data.index)))
-
-    output_data["Proportion"] = 0
-    for sample in tqdm.tqdm(precancer_sample_list):
-        precancer_set = set(mutect_data.loc[(mutect_data["Tumor_Sample_Barcode"] == sample), ["Chromosome", "Start_Position", "End_Position", "Reference_Allele", "Tumor_Seq_Allele1", "Tumor_Seq_Allele2"]].itertuples(index=False, name=None))
-        primary_set = set(mutect_data.loc[(mutect_data["Tumor_Sample_Barcode"] == step00.get_paired_primary(sample)), ["Chromosome", "Start_Position", "End_Position", "Reference_Allele", "Tumor_Seq_Allele1", "Tumor_Seq_Allele2"]].itertuples(index=False, name=None))
-
-        output_data.loc[sample, "Proportion"] = len(precancer_set & primary_set) / len(precancer_set | primary_set)
+    output_data["Overall Survival"] = list(map(lambda x: clinical_data.loc[step00.get_patient(x), "Overall Survival"], list(output_data.index)))
     print(output_data)
 
     for signature in tqdm.tqdm(list(signature_data.columns)):
@@ -96,35 +70,35 @@ if __name__ == "__main__":
     figures = list()
 
     for stage, signature in tqdm.tqdm(list(itertools.product(step00.long_sample_type_list, signature_list))):
-        tmp_data = output_data.loc[(output_data["Stage"] == stage), ["Proportion", signature]]
+        tmp_data = output_data.loc[(output_data["Stage"] == stage), ["Overall Survival", signature]]
         if tmp_data.shape[0] < 3:
             continue
 
-        r, p = scipy.stats.pearsonr(tmp_data[signature], tmp_data["Proportion"])
+        r, p = scipy.stats.pearsonr(tmp_data[signature], tmp_data["Overall Survival"])
 
         fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
 
-        g = seaborn.jointplot(data=tmp_data, x=signature, y="Proportion", kind="reg", height=24, ratio=6, xlim=(-0.1, 1.1), ylim=(-0.1, 1.1))
+        g = seaborn.jointplot(data=tmp_data, x=signature, y="Overall Survival", kind="reg", height=24, ratio=6, xlim=(-0.1, 1.1))
         g.fig.text(0.5, 0.75, "r={0:.3f}, p={1:.3f}".format(r, p), color="k", fontsize="small", horizontalalignment="center", verticalalignment="center", bbox={"alpha": 0.3, "color": "white"}, fontfamily="monospace")
-        g.set_axis_labels("{0} proportion in {1}".format(signature.split("_")[0], signature.split("_")[1]), "Shared Proportion of non-SYN mutations")
+        g.set_axis_labels("{0} proportion in {1}".format(signature.split("_")[0], signature.split("_")[1]), "Overall survival (days)")
 
         figures.append("{1}-{0}.pdf".format(signature, stage))
         g.savefig(figures[-1])
         matplotlib.pyplot.close()
 
     for signature in tqdm.tqdm(signature_list):
-        tmp_data = output_data.loc[:, ["Proportion", signature]]
+        tmp_data = output_data.loc[:, ["Overall Survival", signature]]
 
         if tmp_data.shape[0] < 3:
             continue
 
-        r, p = scipy.stats.pearsonr(tmp_data[signature], tmp_data["Proportion"])
+        r, p = scipy.stats.pearsonr(tmp_data[signature], tmp_data["Overall Survival"])
 
         fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
 
-        g = seaborn.jointplot(data=tmp_data, x=signature, y="Proportion", kind="reg", height=24, ratio=6, xlim=(-0.1, 1.1), ylim=(-0.1, 1.1))
+        g = seaborn.jointplot(data=tmp_data, x=signature, y="Overall Survival", kind="reg", height=24, ratio=6, xlim=(-0.1, 1.1))
         g.fig.text(0.5, 0.75, "r={0:.3f}, p={1:.3f}".format(r, p), color="k", fontsize="small", horizontalalignment="center", verticalalignment="center", bbox={"alpha": 0.3, "color": "white"}, fontfamily="monospace")
-        g.set_axis_labels("{0} proportion in {1}".format(signature.split("_")[0], signature.split("_")[1]), "Shared Proportion of non-SYN mutations")
+        g.set_axis_labels("{0} proportion in {1}".format(signature.split("_")[0], signature.split("_")[1]), "Overall survival (days)")
 
         figures.append("Precancer-{0}.pdf".format(signature))
         g.savefig(figures[-1])
