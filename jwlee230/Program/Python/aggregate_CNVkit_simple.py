7e53a76606718f03a471490dba8aa785c9a3df3a
Jaewoong Lee <230@fumire.moe>
2022-03-02 18:18:08 +0900

    Add simple genome plot with CNVkit

--- jwlee230/Program/Python/aggregate_CNVkit_simple.py
+++ jwlee230/Program/Python/aggregate_CNVkit_simple.py
@@ -1,5 +1,5 @@
 """
-aggregate_CNVkit_genome.py: Aggregate CNVkit results as genome view
+aggregate_CNVkit_simple.py: Aggregate CNVkit results as simple genome view
 """
 import argparse
 import multiprocessing
@@ -11,7 +11,6 @@ import seaborn
 import tqdm
 import step00
 
-input_data = pandas.DataFrame()
 watching = "log2"
 
 
@@ -63,10 +62,6 @@ if __name__ == "__main__":
     group.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
     group.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
 
-    group_sorting = parser.add_mutually_exclusive_group(required=True)
-    group_sorting.add_argument("--patient", help="Sorting by patient first", action="store_true", default=False)
-    group_sorting.add_argument("--type", help="Sorting by type first", action="store_true", default=False)
-
     args = parser.parse_args()
 
     if list(filter(lambda x: not x.endswith(".cnr"), args.input)):
@@ -106,21 +101,21 @@ if __name__ == "__main__":
     input_data["exp"] = numpy.power(2, input_data[watching])
     print(input_data)
 
+    size_data = pandas.read_csv(args.size, sep="\t", header=None, names=["chromosome", "length"]).set_index(keys="chromosome", verify_integrity=True)
+    print(size_data)
+
     chromosome_set = set(input_data["chromosome"])
     chromosome_list = list(filter(lambda x: x in chromosome_set, step00.chromosome_full_list))
     print(chromosome_list)
 
-    size_data = pandas.read_csv(args.size, sep="\t", header=None, names=["chromosome", "length"]).set_index(keys="chromosome", verify_integrity=True)
-    print(size_data)
-
-    stage_set = set(map(step00.get_long_sample_type, args.input))
-    stage_list = list(filter(lambda x: x in stage_set, step00.long_sample_type_list))
+    stage_set = set(map(step00.get_long_sample_type, sample_list))
+    stage_list = list(reversed(list(filter(lambda x: x in stage_set, step00.long_sample_type_list))))
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
     seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
 
-    fig, axs = matplotlib.pyplot.subplots(nrows=3, ncols=len(chromosome_list), sharex="col", sharey="row", figsize=(len(chromosome_list) * 4, len(sample_list) + 16), gridspec_kw={"height_ratios": [8, len(sample_list), 8], "width_ratios": list(map(lambda x: x / step00.big, size_data.loc[chromosome_list, "length"]))})
+    fig, axs = matplotlib.pyplot.subplots(nrows=2 * len(stage_list), ncols=len(chromosome_list), sharex="col", sharey="row", figsize=(len(chromosome_list) * 4, 8 * len(stage_list) * 2), gridspec_kw={"width_ratios": list(map(lambda x: x / step00.big, size_data.loc[chromosome_list, "length"]))})
 
     for i, chromosome in enumerate(chromosome_list):
         chromosome_data = pandas.DataFrame(data=numpy.ones(shape=(len(sample_list), size_data.loc[chromosome, "length"] // step00.big)), index=sample_list, dtype=float)
@@ -129,39 +124,44 @@ if __name__ == "__main__":
             for sample in tqdm.tqdm(sample_list):
                 chromosome_data.loc[sample, :] = pool.starmap(get_chromosome_data, [(sample, chromosome, step * step00.big, (step + 1) * step00.big) for step in list(chromosome_data.columns)])
 
-        for stage in stage_list:
+        for j, stage in enumerate(stage_list):
             stage_sample_list = list(filter(lambda x: step00.get_long_sample_type(x) == stage, sample_list))
             proportion = [1.0 for _ in range(chromosome_data.shape[1])]
-            for j in tqdm.tqdm(range(chromosome_data.shape[1])):
-                proportion[j] = len(list(filter(lambda x: chromosome_data.loc[x, j] >= (1 + args.threshold), stage_sample_list))) / len(stage_sample_list)
-            axs[0][i].plot(proportion, color=step00.stage_color_code[stage], linestyle=step00.stage_linestyle[stage], label=stage)
+            if stage_sample_list:
+                for k in tqdm.tqdm(range(chromosome_data.shape[1])):
+                    proportion[k] = len(list(filter(lambda x: chromosome_data.loc[x, k] >= (1 + args.threshold), stage_sample_list))) / len(stage_sample_list)
 
-        axs[0][i].set_ylim(bottom=0, top=1)
-        axs[0][i].set_xticks([])
-        axs[0][i].set_xlabel(chromosome[3:])
+            axs[j][i].plot(proportion, color=step00.stage_color_code[stage], linestyle=step00.stage_linestyle[stage], label=stage)
 
-        if i == 0:
-            axs[0][i].set_ylabel("Proportion")
-            axs[0][i].legend(loc="upper center")
+            axs[j][i].set_xticks([])
+            axs[j][i].set_ylim(bottom=0, top=1)
+            axs[j][i].set_xlabel(chromosome[3:])
 
-        seaborn.heatmap(data=chromosome_data, vmin=0, center=1, vmax=2, cmap="coolwarm", cbar=False, xticklabels=False, yticklabels=True, ax=axs[1][i])
-        axs[1][i].set_xlabel(chromosome[3:])
+            axs[j][i].plot(proportion, color=step00.stage_color_code[stage], linestyle=step00.stage_linestyle[stage], label=stage)
 
-        for stage in stage_list:
+            axs[j][i].set_xticks([])
+            axs[j][i].set_ylim(bottom=0, top=1)
+            axs[j][i].set_xlabel(chromosome[3:])
+
+            if i == 0:
+                axs[j][i].set_ylabel(stage)
+
+        for j, stage in enumerate(stage_list):
             stage_sample_list = list(filter(lambda x: step00.get_long_sample_type(x) == stage, sample_list))
-            proportion = [1.0 for _ in range(chromosome_data.shape[1])]
-            for j in tqdm.tqdm(range(chromosome_data.shape[1])):
-                proportion[j] = len(list(filter(lambda x: chromosome_data.loc[x, j] <= (1 - args.threshold), stage_sample_list))) / len(stage_sample_list)
-            axs[2][i].plot(proportion, color=step00.stage_color_code[stage], linestyle=step00.stage_linestyle[stage], label=stage)
+            proportion = [1 for _ in range(chromosome_data.shape[1])]
+            if stage_sample_list:
+                for k in tqdm.tqdm(range(chromosome_data.shape[1])):
+                    proportion[k] = len(list(filter(lambda x: chromosome_data.loc[x, k] <= (1 - args.threshold), stage_sample_list))) / len(stage_sample_list)
 
-        axs[2][i].set_ylim(bottom=0, top=1)
-        axs[2][i].invert_yaxis()
-        axs[2][i].set_xticks([])
-        axs[2][i].set_xlabel(chromosome[3:])
+            axs[-1 - j][i].plot(proportion, color=step00.stage_color_code[stage], linestyle=step00.stage_linestyle[stage], label=stage)
 
-        if i == 0:
-            axs[2][i].set_ylabel("Proportion")
-            axs[2][i].legend(loc="lower center")
+            axs[-1 - j][i].set_xticks([])
+            axs[-1 - j][i].set_ylim(bottom=0, top=1)
+            axs[-1 - j][i].set_xlabel(chromosome[3:])
+            axs[-1 - j][i].invert_yaxis()
+
+            if i == 0:
+                axs[-1 - j][i].set_ylabel(stage)
 
     matplotlib.pyplot.tight_layout()
     fig.savefig(args.output + ".pdf")
