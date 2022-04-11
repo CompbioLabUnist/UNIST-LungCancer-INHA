75b5b92791ddc49f474fc9ed03f7e880898fd8e2
Jaewoong Lee <230@fumire.moe>
2022-04-11 16:44:06 +0900

    Upon the issue #18 Add the distribution plot of signatures

---
+++ jwlee230/Program/Python/plot_signature_distribution.py
@@ -0,0 +1,59 @@
+"""
+plot_signature_distribution.py: plot signature distribution
+"""
+import argparse
+import matplotlib
+import matplotlib.pyplot
+import pandas
+import seaborn
+import step00
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Signature TSV file (not necessarily TSV)", type=str)
+    parser.add_argument("clinical", help="Clinidata data CSV file", type=str)
+    parser.add_argument("output", help="Output PDF file", type=str)
+
+    group_subtype = parser.add_mutually_exclusive_group(required=True)
+    group_subtype.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group_subtype.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if not args.clinical.endswith(".csv"):
+        raise ValueError("Clinical data must end with .csv!!")
+    elif not args.output.endswith(".pdf"):
+        raise ValueError("Output must end with .PDF!!")
+
+    input_data = pandas.read_csv(args.input, sep="\t", index_col="Samples")
+    signatures = list(input_data.columns)
+    print(input_data)
+
+    clinical_data = step00.get_clinical_data(args.clinical)
+    print(clinical_data)
+
+    if args.SQC:
+        patients = set(clinical_data.loc[(clinical_data["Histology"] == "SQC")].index)
+    elif args.ADC:
+        patients = set(clinical_data.loc[(clinical_data["Histology"] == "ADC")].index)
+    else:
+        raise Exception("Something went wrong!!")
+    print(sorted(patients))
+
+    input_data = input_data.loc[sorted(filter(lambda x: step00.get_patient(x) in patients, list(input_data.index)), key=step00.sorting_by_type), :]
+    input_data["Stage"] = list(map(step00.get_long_sample_type, list(input_data.index)))
+    order = list(filter(lambda x: x in set(input_data["Stage"]), step00.long_sample_type_list))
+    print(input_data)
+    print(order)
+
+    matplotlib.use("Agg")
+    matplotlib.rcParams.update(step00.matplotlib_parameters)
+    seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
+
+    g = seaborn.pairplot(data=input_data, hue="Stage", hue_order=order, palette=step00.stage_color_code, x_vars=signatures, y_vars=reversed(signatures), kind="scatter", diag_kind="kde", height=8, aspect=1)
+
+    g.tight_layout()
+
+    g.savefig(args.output)
+    matplotlib.pyplot.close(g.fig)
