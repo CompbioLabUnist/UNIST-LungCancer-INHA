c54aaeb300805be0e2e71bbc2451895c551e6269
Jaewoong Lee <230@fumire.moe>
2022-06-10 14:47:42 +0900

    Server has been upgrade

--- jwlee230/Program/Bash/remove_CR.bash
+++ jwlee230/Program/Bash/remove_CR.bash
@@ -3,9 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if (($# == 2)); then
-    cp $1 /tmpfs/${1##*/}
-    samtools view -h /tmpfs/${1##*/} | sed "s/\r//" | samtools view -b -o /tmpfs/${2##*/}
-    cp /tmpfs/${2##*/} $2
+    samtools view -h $1 | sed "s/\r//" | samtools view -b -o $2
 else
     echo "This script will work if and only if it has 2 arguments"
 fi
