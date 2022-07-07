7f6bfe35aae94959b3764f016215bfd5c15a48c5
Jaewoong Lee <230@fumire.moe>
2022-07-07 14:45:08 +0900

    Add venn diagram (#32)

---
+++ jwlee230/Program/Python/compare_PathSeq_venn.py
@@ -0,0 +1,53 @@
+"""
+compare_PathSeq_venn.py: compare Pathseq WES vs. WTS with venn diagram
+"""
+import argparse
+import matplotlib
+import matplotlib.pyplot
+import pandas
+import tqdm
+import venn
+import step00
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="PathSeq results TSV file(s)", type=str, nargs="+")
+    parser.add_argument("output", help="Output PDF file", type=str)
+    parser.add_argument("--annotation", help="Annotation for figure", type=str, nargs="+", required=True)
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if list(filter(lambda x: not x.endswith(".tsv"), args.input)):
+        raise ValueError("INPUT must end with .TSV!!")
+    elif not args.output.endswith(".pdf"):
+        raise ValueError("Output must end with .PDF!!")
+    elif len(args.input) != len(args.annotation):
+        raise ValueError("INPUT must have same ANNOTATION!!")
+
+    output_data = dict()
+    for input_file, input_name in tqdm.tqdm(zip(args.input, args.annotation)):
+        input_data = pandas.read_csv(input_file, sep="\t", index_col=0)
+        output_data[input_name] = set(list(input_data.columns)[:-1])
+
+    matplotlib.use("Agg")
+    matplotlib.rcParams.update(step00.matplotlib_parameters)
+
+    fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
+
+    if len(args.input) == 6:
+        venn.pseudovenn(output_data, ax=ax, fmt=step00.venn_format, fontsize=step00.matplotlib_parameters["legend.fontsize"], legend_loc="upper left")
+    elif 0 < len(args.input) < 6:
+        venn.venn(output_data, ax=ax, fmt=step00.venn_format, fontsize=step00.matplotlib_parameters["legend.fontsize"], legend_loc="upper left")
+    else:
+        raise Exception("Something went wrong!!")
+
+    matplotlib.pyplot.tight_layout()
+
+    fig.savefig(args.output)
+    matplotlib.pyplot.close(fig)
