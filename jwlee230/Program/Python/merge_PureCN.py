16b73a4ceb892d6b1318487c3cf401b963d0ce76
Jaewoong Lee <230@fumire.moe>
2022-09-02 09:12:01 +0900

    Add PureCN to unified pipeline (#20)

---
+++ jwlee230/Program/Python/merge_PureCN.py
@@ -0,0 +1,36 @@
+"""
+merge_PureCN.py: merge PureCN csv results as TSV
+"""
+import argparse
+import multiprocessing
+import pandas
+import step00
+
+
+def get_data(file_name: str) -> pandas.DataFrame:
+    d = pandas.read_csv(file_name, quoting=2, index_col="Sampleid")
+    return d
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="PureCN output CSV file(s)", type=str, nargs="+")
+    parser.add_argument("output", help="Output TSV file", type=str)
+    parser.add_argument("--cpus", help="Number of CPUs to use", type=int, default=1)
+
+    args = parser.parse_args()
+
+    if list(filter(lambda x: not x.endswith(".csv"), args.input)):
+        raise ValueError("INPUT must end with .CSV!!")
+    elif not args.output.endswith(".tsv"):
+        raise ValueError("Output must end with .TSV!!")
+    elif args.cpus < 1:
+        raise ValueError("CPUs must be positive!!")
+
+    with multiprocessing.Pool(args.cpus) as pool:
+        input_data = pandas.concat(objs=pool.map(get_data, args.input), axis="index", copy=False)
+        input_data["Stage"] = pool.map(step00.get_long_sample_type, list(input_data.index))
+    print(input_data)
+
+    input_data.to_csv(args.output, sep="\t")
--- jwlee230/Program/Python/merge_PureCN_2.py
+++ jwlee230/Program/Python/merge_PureCN_2.py
@@ -1,5 +1,5 @@
 """
-merge_sequenza_2.py: merge Sequenza
+merge_PureCN.py: merge PureCN csv results as TSV
 """
 import argparse
 import multiprocessing
@@ -8,23 +8,24 @@ import step00
 
 
 def get_data(file_name: str) -> pandas.DataFrame:
-    data = pandas.read_csv(file_name, sep="\t").dropna(axis="index").dropna(axis="index")
-    data.columns = ["chromosome", "start", "end"] + list(data.columns)[3:]
-    data["Sample"] = file_name.split("/")[-2]
+    data = pandas.read_csv(file_name, sep="\t")
+    data["chrom"] = list(map(lambda x: "chrX" if (str(x) == "23") else ("chr" + str(x)), data["chrom"]))
+    data.columns = ["Sample", "chromosome", "start", "end"] + list(data.columns)[4:]
+    data["Sample"] = step00.get_id(file_name)
     return data
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("input", help="Sequenza output alternative_solutions.txt file(s)", type=str, nargs="+")
+    parser.add_argument("input", help="PureCN seg.TSV file(s)", type=str, nargs="+")
     parser.add_argument("output", help="Output TSV file", type=str)
     parser.add_argument("--cpus", help="Number of CPUs to use", type=int, default=1)
 
     args = parser.parse_args()
 
-    if list(filter(lambda x: not x.endswith(".txt"), args.input)):
-        raise ValueError("INPUT must end with .TXT!!")
+    if list(filter(lambda x: not x.endswith(".tsv"), args.input)):
+        raise ValueError("INPUT must end with .TSV!!")
     elif not args.output.endswith(".tsv"):
         raise ValueError("Output must end with .TSV!!")
     elif args.cpus < 1:
@@ -32,6 +33,7 @@ if __name__ == "__main__":
 
     with multiprocessing.Pool(args.cpus) as pool:
         input_data = pandas.concat(objs=pool.map(get_data, args.input), axis="index", copy=False, ignore_index=True)
+        print(input_data)
         input_data["Patient"] = pool.map(step00.get_patient, input_data["Sample"])
         input_data["Stage"] = pool.map(step00.get_long_sample_type, input_data["Sample"])
     print(input_data)
