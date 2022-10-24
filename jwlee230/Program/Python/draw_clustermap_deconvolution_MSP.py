aa72175456e60f80042c40bb4810cc8b7b93cdc8
Jaewoong Lee <230@fumire.moe>
2022-10-24 19:26:57 +0900

    Update upon the issue (#12 & #31)

--- jwlee230/Program/Python/draw_clustermap_deconvolution_MSP.py
+++ jwlee230/Program/Python/draw_clustermap_deconvolution_MSP.py
@@ -103,7 +103,10 @@ if __name__ == "__main__":
         if selected_data.shape[1] < 2:
             continue
 
-        g = seaborn.clustermap(data=selected_data, figsize=(32, 18), row_cluster=True, col_cluster=True, cbar_pos=(-0.04, 0.2, 0.02, 0.6), col_colors=list(color_data.loc[selected_samples, MSP].to_numpy()), xticklabels=False, yticklabels=False, square=False, z_score=0, cmap="coolwarm", center=0, robust=True)
+        tmp_color = pandas.DataFrame()
+        tmp_color["MSP"] = color_data.loc[selected_samples, MSP]
+
+        g = seaborn.clustermap(data=selected_data, figsize=(32, 18), row_cluster=True, col_cluster=True, cbar_pos=(-0.04, 0.2, 0.02, 0.6), col_colors=tmp_color, xticklabels=False, yticklabels=False, square=False, z_score=0, cmap="coolwarm", center=0, robust=True)
 
         g.ax_heatmap.set_xlabel(f"{selected_data.shape[1]} {stage} samples")
         g.ax_heatmap.set_ylabel(f"{selected_data.shape[0]} cell types")
