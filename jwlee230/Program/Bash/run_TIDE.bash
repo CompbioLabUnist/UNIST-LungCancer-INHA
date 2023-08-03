e9e53c7b7bff2de8f05c482c3914fe1bbf462abf
Jaewoong Lee <230@fumire.moe>
2023-08-03 19:23:05 +0900

    Resolve Python dependency

--- jwlee230/Program/Bash/run_TIDE.bash
+++ jwlee230/Program/Bash/run_TIDE.bash
@@ -4,10 +4,11 @@ IFS=$'\n\t'
 
 if (($# == 2)); then
     cd $HOME
-    python3 -m venv . --upgrade-deps
+    python3 -m venv .
     source bin/activate
-    pip3 install --upgrade wheel pip numpy==1.18.5 Cython
-    pip3 install pandas==0.25.3 tidepy
+    pip3 install --upgrade wheel pip
+    pip3 install numpy==1.25.2 Cython==3.0.0
+    pip3 install pandas==2.0.3 tidepy==1.3.8
     tidepy --output $2 --cancer "NSCLC" --pretreat --force_normalize $1
 else
     echo "This script will work if and only if it has 2 arguments"
