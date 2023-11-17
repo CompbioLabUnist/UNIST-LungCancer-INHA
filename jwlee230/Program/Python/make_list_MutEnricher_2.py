7bed4e29d94381e7a0cfe25ed13e477ae4effa5b
Jaewoong Lee <230@fumire.moe>
2023-11-18 01:54:24 +0900

    Add MutEnricher selection
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

---
+++ jwlee230/Program/Python/make_list_MutEnricher_2.py
@@ -0,0 +1,50 @@
+"""
+make_list_MutEnricher_2.py: Make VCF file list for MutEnricher input with selection
+"""
+import argparse
+import csv
+import step00
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Input VCF(.GZ) files", type=str, nargs="+")
+    parser.add_argument("clinical", help="Clinical data CSV file", type=str)
+    parser.add_argument("output", help="Output TSV file", type=str)
+    parser.add_argument("--patient", help="List of patients to select", type=str, nargs="+")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if list(filter(lambda x: (not x.endswith(".vcf")) and (not x.endswith(".vcf.gz")), args.input)):
+        raise ValueError("INPUT must end with .VCF(.GZ)!!")
+    elif not args.clinical.endswith(".csv"):
+        raise ValueError("Clinical must end with .CSV!!")
+    elif not args.output.endswith(".tsv"):
+        raise ValueError("OUTPUT must end with .TSV!!")
+
+    clinical_data = step00.get_clinical_data(args.clinical)
+    print(clinical_data)
+
+    patients = set(args.patient)
+    print(patients)
+
+    if args.SQC:
+        patients &= set(clinical_data.loc[(clinical_data["Histology"] == "SQC")].index)
+    elif args.ADC:
+        patients &= set(clinical_data.loc[(clinical_data["Histology"] == "ADC")].index)
+    else:
+        raise Exception("Something went wrong!!")
+    print(patients)
+
+    # args.input = list(filter(lambda x: (step00.get_patient(step00.get_id(x)) in patients) and (step00.get_long_sample_type(step00.get_id(x)) == "Primary"), args.input))
+    args.input = list(filter(lambda x: step00.get_patient(step00.get_id(x)) in patients, args.input))
+    args.input.sort()
+    print(args.input)
+
+    with open(args.output, "w", newline="") as f:
+        writer = csv.writer(f, delimiter="\t")
+        writer.writerows([(i, step00.get_id(i)) for i in args.input])
