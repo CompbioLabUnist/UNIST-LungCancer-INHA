4480af140a5be4daa1ad99492400612bbd162925
Jaewoong Lee <230@fumire.moe>
2023-12-05 17:19:36 +0900

    Add gene expression with MSP (#7 & #31)
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- jwlee230/Program/Python/draw_bar_plots_MSP_sample.py
+++ jwlee230/Program/Python/draw_bar_plots_MSP_sample.py
@@ -7,28 +7,44 @@ import multiprocessing
 import tarfile
 import matplotlib
 import matplotlib.pyplot
+import numpy
 import pandas
+import seaborn
+import statannotations.Annotator
 import tqdm
 import step00
 
 expression_data = pandas.DataFrame()
-order = ["Lower", "Higher"]
+metadata = pandas.DataFrame()
 
 
-def run(sample: str, gene: str) -> str:
+def run(MSP: str, gene: str) -> str:
+    lower_bound, higher_bound = numpy.quantile(clinical_data[MSP], args.percentage), numpy.quantile(clinical_data[MSP], 1.0 - args.percentage)
+
+    expression_samples = set(expression_data.index)
+
+    lower_precancer_list = list(filter(lambda x: x in expression_samples, list(clinical_data.loc[(clinical_data[MSP] <= lower_bound), f"{MSP}-sample"])))
+    higher_precancer_list = list(filter(lambda x: x in expression_samples, list(clinical_data.loc[(clinical_data[MSP] >= higher_bound), f"{MSP}-sample"])))
+    lower_primary_list = list(filter(lambda x: x in expression_samples, list(map(step00.get_paired_primary, lower_precancer_list))))
+    higher_primary_list = list(filter(lambda x: x in expression_samples, list(map(step00.get_paired_primary, higher_precancer_list))))
+
+    raw_output_data = list()
+    raw_output_data += [(sample, "Lower", "Precancer", expression_data.loc[sample, gene]) for sample in lower_precancer_list]
+    raw_output_data += [(sample, "Lower", "Primary", expression_data.loc[sample, gene]) for sample in lower_primary_list]
+    raw_output_data += [(sample, "Higher", "Precancer", expression_data.loc[sample, gene]) for sample in higher_precancer_list]
+    raw_output_data += [(sample, "Higher", "Primary", expression_data.loc[sample, gene]) for sample in higher_primary_list]
+
+    output_data = pandas.DataFrame(raw_output_data, columns=["Sample", "Lower/Higher", "PRE/PRI", "Expression"])
+
     fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
 
-    matplotlib.pyplot.bar(0, expression_data.loc[sample, gene], width=0.8, color=step00.stage_color_code[step00.get_long_sample_type(sample)], label=step00.get_long_sample_type(sample))
-    matplotlib.pyplot.bar(1, expression_data.loc[step00.get_paired_primary(sample), gene], width=0.8, color=step00.stage_color_code["Primary"], label="Primary")
+    seaborn.violinplot(data=output_data, x="Lower/Higher", y="Expression", hue="PRE/PRI", order=["Lower", "Higher"], hue_order=["Precancer", "Primary"], palette={"Precancer": "tab:pink", "Primary": "gray"}, innter="box", linewidth=5, cut=1, ax=ax)
+    statannotations.Annotator.Annotator(ax, [(("Lower", "Precancer"), ("Lower", "Primary")), (("Higher", "Precancer"), ("Higher", "Primary")), (("Lower", "Precancer"), ("Higher", "Precancer")), (("Lower", "Primary"), ("Higher", "Primary"))], data=output_data, x="Lower/Higher", y="Expression", hue="PRE/PRI", order=["Lower", "Higher"], hue_order=["Precancer", "Primary"]).configure(test="Mann-Whitney", text_format="star", loc="inside", verbose=0, comparisons_correction=None).apply_and_annotate()
 
-    matplotlib.pyplot.xticks([0, 1], [step00.get_long_sample_type(sample), "Primary"])
-    matplotlib.pyplot.xlabel(f"{step00.get_patient(sample)}")
     matplotlib.pyplot.ylabel(f"{gene} expression")
-    matplotlib.pyplot.grid(True)
-    matplotlib.pyplot.legend()
     matplotlib.pyplot.tight_layout()
 
-    fig_name = f"{sample}-{gene}.pdf"
+    fig_name = f"{MSP}-{gene}.pdf"
     fig.savefig(fig_name)
     matplotlib.pyplot.close(fig)
 
@@ -44,7 +60,12 @@ if __name__ == "__main__":
     parser.add_argument("output", help="Output TAR file", type=str)
     parser.add_argument("--r", help="r-value threshold", type=float, default=0.6)
     parser.add_argument("--slope", help="Slope threshold", type=float, default=100)
+    parser.add_argument("--percentage", help="Percentage of patients to include", type=float, default=0.1)
     parser.add_argument("--cpus", help="Number of CPUs to use", type=int, default=1)
+
+    group_subtype = parser.add_mutually_exclusive_group(required=True)
+    group_subtype.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
+    group_subtype.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
 
     args = parser.parse_args()
 
@@ -60,15 +81,25 @@ if __name__ == "__main__":
         raise ValueError("r-value must be in (0, 1)!!")
     elif args.slope <= 0:
         raise ValueError("Slope must be positive!!")
+    elif not (0.0 < args.percentage < 0.5):
+        raise ValueError("Percentage must be (0.0, 0.5)!!")
     elif args.cpus < 1:
         raise ValueError("Number of CPUs must be positive!!")
 
     input_data = pandas.read_csv(args.input, sep="\t", index_col=0)
     print(input_data)
 
-    clinical_data: pandas.DataFrame = pandas.read_csv(args.clinical, sep="\t", index_col=0)
-    patients = set(clinical_data.index)
+    clinical_data = pandas.read_csv(args.clinical, sep="\t", index_col=0)
     print(clinical_data)
+
+    if args.SQC:
+        clinical_data = clinical_data.loc[(clinical_data["Histology"] == "SQC")]
+    elif args.ADC:
+        clinical_data = clinical_data.loc[(clinical_data["Histology"] == "ADC")]
+    else:
+        raise Exception("Something went wrong!!")
+    patients = set(clinical_data.index)
+    print(sorted(patients))
 
     expression_data = pandas.read_csv(args.expression, sep="\t", index_col=0).T
     expression_data = expression_data.loc[list(filter(lambda x: step00.get_patient(x) in patients, list(expression_data.index))), :]
@@ -79,19 +110,16 @@ if __name__ == "__main__":
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
+    seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
 
     genes = set()
     for MSP in tqdm.tqdm(step00.sharing_columns):
-        genes |= set(input_data.loc[(input_data[f"CIS+AIS-{MSP}-slope"] > args.slope) & ((input_data[f"CIS+AIS-{MSP}-r"] > args.r) | (input_data[f"CIS+AIS-{MSP}-r"] < (-1 * args.r)))].index)
-    print(len(genes))
-
-    samples = list(expression_data.index)
-    precancer_samples = list(filter(lambda x: (step00.get_long_sample_type(x) != "Primary") and (step00.get_paired_primary(x) in samples), samples))
-    print(len(precancer_samples))
+        genes |= set(input_data.loc[(input_data[f"Precancer-{MSP}-slope"] > args.slope) & ((input_data[f"Precancer-{MSP}-r"] > args.r) | (input_data[f"Precancer-{MSP}-r"] < (-1 * args.r)))].index)
+    print(len(genes), sorted(genes))
 
     figures = list()
     with multiprocessing.Pool(args.cpus) as pool:
-        figures = list(pool.starmap(run, itertools.product(precancer_samples, sorted(genes))))
+        figures = list(pool.starmap(run, itertools.product(step00.sharing_columns, sorted(genes))))
 
     with tarfile.open(args.output, "w") as tar:
         for f in tqdm.tqdm(figures):
