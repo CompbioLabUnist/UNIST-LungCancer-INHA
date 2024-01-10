24f06af115e7faaa7da71a20d7092fd51d55b102
Jaewoong Lee <230@fumire.moe>
2024-01-10 16:30:58 +0900

    Update to scatter plot (#20 & #31)
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- jwlee230/Program/Python/aggregate_CNV_MutationSharedProportion_precancer.py
+++ jwlee230/Program/Python/aggregate_CNV_MutationSharedProportion_precancer.py
@@ -2,12 +2,15 @@
 aggregate_CNV_MutationSharedProportion_precancer.py: Aggregate CNV results w/ MSP as precancer vs. primary
 """
 import argparse
+import itertools
 import multiprocessing
 import tarfile
 import matplotlib
 import matplotlib.pyplot
 import numpy
 import pandas
+import scipy.stats
+import seaborn
 import tqdm
 import step00
 
@@ -107,56 +110,73 @@ if __name__ == "__main__":
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
+    seaborn.set_theme(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
 
     figures = list()
     for MSP in tqdm.tqdm(step00.sharing_columns):
         lower_bound, higher_bound = numpy.quantile(clinical_data[MSP], args.percentage), numpy.quantile(clinical_data[MSP], 1.0 - args.percentage)
 
-        lower_precancer_list = list(clinical_data.loc[(clinical_data[MSP] <= lower_bound), f"{MSP}-sample"])
+        lower_precancer_list = list(clinical_data.loc[(clinical_data[MSP] < lower_bound), f"{MSP}-sample"])
         higher_precancer_list = list(clinical_data.loc[(clinical_data[MSP] >= higher_bound), f"{MSP}-sample"])
 
         lower_primary_list = list(map(step00.get_paired_primary, lower_precancer_list))
         higher_primary_list = list(map(step00.get_paired_primary, higher_precancer_list))
 
-        fig, axs = matplotlib.pyplot.subplots(ncols=len(chromosome_list), nrows=2, sharex="col", sharey="all", figsize=(64, 36), gridspec_kw={"width_ratios": list(map(lambda x: x / step00.big, size_data.loc[chromosome_list, "length"]))})
-
-        for i, chromosome in enumerate(chromosome_list):
+        raw_drawing_data = list()
+        for i, chromosome in tqdm.tqdm(list(enumerate(chromosome_list)), leave=False):
             chromosome_data = pandas.DataFrame(data=numpy.ones(shape=(len(lower_precancer_list) + len(lower_primary_list), size_data.loc[chromosome, "length"] // step00.big)), index=lower_precancer_list + lower_primary_list, dtype=float)
 
             with multiprocessing.Pool(args.cpus) as pool:
                 for sample in lower_precancer_list + lower_primary_list:
                     chromosome_data.loc[sample, :] = pool.starmap(get_chromosome_data, [(sample, chromosome, step * step00.big, (step + 1) * step00.big) for step in list(chromosome_data.columns)])
 
-            axs[0][i].errorbar(range(len(chromosome_data.columns)), numpy.mean(chromosome_data.loc[lower_primary_list, :], axis=0), yerr=numpy.std(chromosome_data.loc[lower_primary_list, :], axis=0), fmt="none", ecolor="gray", elinewidth=1)
-            axs[0][i].errorbar(range(len(chromosome_data.columns)), numpy.mean(chromosome_data.loc[lower_precancer_list, :], axis=0), yerr=numpy.std(chromosome_data.loc[lower_precancer_list, :], axis=0) / 2, fmt="none", ecolor="tab:pink", elinewidth=1)
+            for (precancer_sample, primary_sample), column in tqdm.tqdm(list(itertools.product(zip(lower_precancer_list, lower_primary_list), list(chromosome_data.columns))), leave=False):
+                raw_drawing_data.append((chromosome_data.loc[precancer_sample, column], chromosome_data.loc[primary_sample, column]))
 
-            axs[0][i].set_xticks([])
-            axs[0][i].set_xlabel(chromosome[3:])
-            axs[0][i].grid(True)
+        drawing_data = pandas.DataFrame(raw_drawing_data, columns=["Precancer", "Primary"])
+        minimum, maximum = min(drawing_data.min()) - 0.1, max(drawing_data.max()) + 0.1
+        r, p = scipy.stats.pearsonr(drawing_data["Precancer"], drawing_data["Primary"])
 
-            if i == 0:
-                axs[0][i].set_ylabel("Copy number")
+        fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
 
-        for i, chromosome in enumerate(chromosome_list):
+        seaborn.regplot(data=drawing_data, x="Primary", y="Precancer", color="tab:blue", ax=ax)
+        matplotlib.pyplot.axline((0, 0), slope=1, color="k", linestyle="--", linewidth=2)
+
+        matplotlib.pyplot.xlim((minimum, maximum))
+        matplotlib.pyplot.ylim((minimum, maximum))
+        matplotlib.pyplot.title(f"r={r:.3f}, p={p:.3f}")
+        matplotlib.pyplot.tight_layout()
+
+        figures.append(f"{MSP}-lower.pdf")
+        fig.savefig(figures[-1])
+        matplotlib.pyplot.close(fig)
+
+        raw_drawing_data = list()
+        for i, chromosome in tqdm.tqdm(list(enumerate(chromosome_list)), leave=False):
             chromosome_data = pandas.DataFrame(data=numpy.ones(shape=(len(higher_precancer_list) + len(higher_primary_list), size_data.loc[chromosome, "length"] // step00.big)), index=higher_precancer_list + higher_primary_list, dtype=float)
 
             with multiprocessing.Pool(args.cpus) as pool:
                 for sample in higher_precancer_list + higher_primary_list:
                     chromosome_data.loc[sample, :] = pool.starmap(get_chromosome_data, [(sample, chromosome, step * step00.big, (step + 1) * step00.big) for step in list(chromosome_data.columns)])
 
-            axs[1][i].errorbar(range(len(chromosome_data.columns)), numpy.mean(chromosome_data.loc[higher_primary_list, :], axis=0), yerr=numpy.std(chromosome_data.loc[higher_primary_list, :], axis=0), fmt="none", ecolor="gray", elinewidth=1)
-            axs[1][i].errorbar(range(len(chromosome_data.columns)), numpy.mean(chromosome_data.loc[higher_precancer_list, :], axis=0), yerr=numpy.std(chromosome_data.loc[higher_precancer_list, :], axis=0) / 2, fmt="none", ecolor="tab:pink", elinewidth=1)
+            for (precancer_sample, primary_sample), column in tqdm.tqdm(list(itertools.product(zip(higher_precancer_list, higher_primary_list), list(chromosome_data.columns))), leave=False):
+                raw_drawing_data.append((chromosome_data.loc[precancer_sample, column], chromosome_data.loc[primary_sample, column]))
 
-            axs[1][i].set_xticks([])
-            axs[1][i].set_xlabel(chromosome[3:])
-            axs[1][i].grid(True)
+        drawing_data = pandas.DataFrame(raw_drawing_data, columns=["Precancer", "Primary"])
+        minimum, maximum = min(drawing_data.min()) - 0.1, max(drawing_data.max()) + 0.1
+        r, p = scipy.stats.pearsonr(drawing_data["Precancer"], drawing_data["Primary"])
 
-            if i == 0:
-                axs[1][i].set_ylabel("Copy number")
+        fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
 
+        seaborn.regplot(data=drawing_data, x="Primary", y="Precancer", color="tab:blue", ax=ax)
+        matplotlib.pyplot.axline((0, 0), slope=1, color="k", linestyle="--", linewidth=2)
+
+        matplotlib.pyplot.xlim((minimum, maximum))
+        matplotlib.pyplot.ylim((minimum, maximum))
+        matplotlib.pyplot.title(f"r={r:.3f}, p={p:.3f}")
         matplotlib.pyplot.tight_layout()
 
-        figures.append(f"{MSP}.pdf")
+        figures.append(f"{MSP}-higher.pdf")
         fig.savefig(figures[-1])
         matplotlib.pyplot.close(fig)
 
