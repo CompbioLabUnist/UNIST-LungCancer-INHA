0ae27f93943409732782186e58559bba79499c54
Jaewoong Lee <230@fumire.moe>
2024-10-09 16:29:26 +0900

    Update KM plot
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- jwlee230/Program/Python/draw_KM_SharedProportion.py
+++ jwlee230/Program/Python/draw_KM_SharedProportion.py
@@ -2,8 +2,8 @@
 draw_KM_SharedProportion.py: Draw Kaplan-Meier plot with mutation shared proportion
 """
 import argparse
+import itertools
 import tarfile
-import typing
 import lifelines
 import lifelines.statistics
 import matplotlib
@@ -23,16 +23,12 @@ if __name__ == "__main__":
 
     parser.add_argument("input", help="Mutation Sharing Proportion input TSV file", type=str)
     parser.add_argument("output", help="Output TAR file", type=str)
-    parser.add_argument("--column", help="Column for Mutation Shared Proportion", choices=step00.sharing_columns, default=step00.sharing_columns[0])
     parser.add_argument("--cutting", help="Cutting follow-up up to 5 year", action="store_true", default=False)
+    parser.add_argument("--percentage", help="Percentage threshold", type=float, default=0.25)
 
     group_subtype = parser.add_mutually_exclusive_group(required=True)
     group_subtype.add_argument("--SQC", help="Get SQC patient only", action="store_true", default=False)
     group_subtype.add_argument("--ADC", help="Get ADC patient only", action="store_true", default=False)
-
-    group_divide = parser.add_mutually_exclusive_group(required=True)
-    group_divide.add_argument("--median", help="Divide as median", action="store_true", default=False)
-    group_divide.add_argument("--mean", help="Divide as mean", action="store_true", default=False)
 
     args = parser.parse_args()
 
@@ -41,34 +37,22 @@ if __name__ == "__main__":
     elif not args.output.endswith(".tar"):
         raise ValueError("Output must end with .TAR!!")
 
-    input_data: pandas.DataFrame = pandas.read_csv(args.input, sep="\t", index_col=0)
-    print(input_data)
+    clinical_data = pandas.read_csv(args.input, sep="\t", index_col=0)
+    print(clinical_data)
 
     if args.SQC:
-        input_data = input_data.loc[(input_data["Histology"] == "SQC")]
+        clinical_data = clinical_data.loc[(clinical_data["Histology"] == "SQC")]
     elif args.ADC:
-        input_data = input_data.loc[(input_data["Histology"] == "ADC")]
+        clinical_data = clinical_data.loc[(clinical_data["Histology"] == "ADC")]
     else:
         raise Exception("Something went wrong!!")
-    patients = set(input_data.index)
+    patients = set(clinical_data.index)
     print(len(patients))
 
     if args.cutting:
         for column in tqdm.tqdm(survival_columns):
-            input_data[column] = list(map(lambda x: threshold if (x > threshold) else x, input_data[column]))
-    print(input_data)
-
-    if args.median:
-        threshold = numpy.median(input_data[args.column])
-    elif args.mean:
-        threshold = numpy.mean(input_data[args.column])
-    else:
-        raise Exception("Something went wrong!!")
-
-    lower_data = input_data.loc[(input_data[args.column] <= threshold)]
-    higher_data = input_data.loc[(input_data[args.column] > threshold)]
-    print(lower_data)
-    print(higher_data)
+            clinical_data[column] = list(map(lambda x: threshold if (x > threshold) else x, clinical_data[column]))
+    print(clinical_data)
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
@@ -76,35 +60,36 @@ if __name__ == "__main__":
 
     figures = list()
 
-    for column in tqdm.tqdm(survival_columns):
-        if column == "Recurrence-Free Survival":
-            lower_events: typing.Optional[typing.List[bool]] = list(map(lambda x: x == 1, lower_data["Recurrence"]))
-            higher_events: typing.Optional[typing.List[bool]] = list(map(lambda x: x == 1, higher_data["Recurrence"]))
-        elif column == "Overall Survival":
-            lower_events = None
-            higher_events = None
-        else:
-            raise ValueError("Something went wrong!!")
+    for MSP, column in tqdm.tqdm(list(itertools.product(step00.sharing_columns[:2], survival_columns))):
+        precancer_list = sorted(clinical_data[f"{MSP}-sample"], key=lambda x: clinical_data.loc[step00.get_patient(x), MSP])
+        primary_list = list(map(step00.get_paired_primary, precancer_list))
+        patient_list = list(map(step00.get_patient, precancer_list))
 
-        fig, ax = matplotlib.pyplot.subplots(figsize=(32, 18))
+        MSP_Q1 = numpy.quantile(clinical_data[MSP], args.percentage)
+        MSP_Q3 = numpy.quantile(clinical_data[MSP], 1.0 - args.percentage)
+
+        MSP_Q_list = list(map(lambda x: "PSM-L" if (clinical_data.loc[x, MSP] <= MSP_Q1) else ("PSM-H" if (clinical_data.loc[x, MSP] >= MSP_Q3) else "None"), patient_list))
+        MSP_L_list = list(map(lambda x: x[1], list(filter(lambda x: x[0] == "PSM-L", zip(MSP_Q_list, precancer_list)))))
+        MSP_H_list = list(map(lambda x: x[1], list(filter(lambda x: x[0] == "PSM-H", zip(MSP_Q_list, precancer_list)))))
+
+        fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
 
         kmf = lifelines.KaplanMeierFitter()
 
-        kmf.fit(lower_data[column], event_observed=lower_events, label=f"Lower Shared Proportion ({len(lower_data)} patients)")
+        kmf.fit(clinical_data.loc[list(map(step00.get_patient, MSP_L_list)), column], label=f"PSM-L ({len(MSP_L_list)} patients)")
         kmf.plot(ax=ax, ci_show=False, c="tab:blue")
 
-        kmf.fit(higher_data[column], event_observed=higher_events, label=f"Higher Shared Proportion ({len(higher_data)} patients)")
+        kmf.fit(clinical_data.loc[list(map(step00.get_patient, MSP_H_list)), column], label=f"PSM-H ({len(MSP_H_list)} patients)")
         kmf.plot(ax=ax, ci_show=False, c="tab:red")
 
-        p_value = lifelines.statistics.logrank_test(lower_data[column], higher_data[column], event_observed_A=lower_events, event_observed_B=higher_events).p_value
-        matplotlib.pyplot.text(max(input_data[column]) / 2, 1.0, f"p={p_value:.3f}", color="k", fontsize="small", horizontalalignment="center", verticalalignment="center", bbox={"alpha": 0.3, "color": "white"}, fontfamily="monospace")
+        p_value = lifelines.statistics.logrank_test(clinical_data.loc[list(map(step00.get_patient, MSP_L_list)), column], clinical_data.loc[list(map(step00.get_patient, MSP_H_list)), column]).p_value
+        matplotlib.pyplot.text(max(clinical_data[column]) / 2, 0.8, f"p={p_value:.3f}", color="black", fontsize="small", horizontalalignment="center", verticalalignment="center")
 
         matplotlib.pyplot.xlabel(f"{column} (Days)")
         matplotlib.pyplot.ylabel("Survival Rate")
-        matplotlib.pyplot.title(f"Lower/Higher Threshold: {threshold:.3f}")
         matplotlib.pyplot.tight_layout()
 
-        figures.append(f"{column.replace(' ', '-')}.pdf")
+        figures.append(f"{MSP}-{column}.pdf")
         matplotlib.pyplot.savefig(figures[-1])
         matplotlib.pyplot.close()
 
